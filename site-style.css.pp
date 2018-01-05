0b1bdc5635e877282bda90805cc18c187bae7092
Sancho McCann <sanchom@gmail.com>
2018-01-04 18:50:04 -0800

    Bumped font size on small screens.

--- site-style.css.pp
+++ site-style.css.pp
@@ -4,7 +4,7 @@
 ◊(define width-for-max-font 920)
 ◊(define size-steps 6)
 ◊(define max-font-size 18)
-◊(define font-size-on-small-screens 14)
+◊(define font-size-on-small-screens 16)
 ◊(define small-screen-trigger (- width-for-max-font (* (- size-steps 1) 40)))
 
 ◊(define link-color "#3f75a2")
