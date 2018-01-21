7744af10174dd5fdced68161e141bcf91676c5f3
Sancho McCann <sanchom@gmail.com>
2018-01-20 21:34:02 -0800

    Working implementation of the ◊reg tag.

--- canadian-aviation-regulations/pollen.rkt
+++ canadian-aviation-regulations/pollen.rkt
@@ -19,22 +19,59 @@
       (txexpr 'b empty (list (txexpr 'em empty content)))
       input-txexpr))
 
-; TODO(sanchom): Process each section recursively, processing the xexpressions using #:txexpr-proc.
-(define (car #:sec sec #:subsec [subsec #f] #:para [para #f] #:subpara [subpara #f] #:clause [clause #f] #:subclause [subclause #f])
-  (define tmp-content
-    (xexpr-path-first `(Section (Code ,(format "se=\"~a\"" sec))) cars-xml))
+(define (reg #:sec sec #:subsec [subsec #f])
   (define section-content
-    (if subsec (xexpr-path-first `(Subsection (Code ,(format "se=\"~a\",ss=\"~a\"" sec subsec))) tmp-content)
-        tmp-content))
-  
-  (define content
-    (xexpr-path-first '(Text) section-content))
-  (txexpr 'span empty (decode-elements
-                       (list (txexpr 'b empty (list (if subsec (format "~a (~a) " sec subsec)
-                                                        (format "~a " sec))))
-                             (if content content "--- Failed to parse xml ---"))
-                       #:txexpr-proc replace-definedtermen ; Replacing (DefinedTermEn content) with (b (em content))
-                       )))
+    (xexpr-path-first `(Section (Code ,(format "se=\"~a\"" sec))) cars-xml))
+  (decode (parse-section-content section-content #:sec sec #:subsec subsec)
+          #:txexpr-proc replace-definedtermen))
+
+(define (parse-section-content content #:sec sec #:subsec [subsec #f])
+  (define (parse-subsec-specialized content)
+    (parse-subsec-content content sec))    
+  (if subsec
+      (txexpr 'div empty `(,(parse-subsec-specialized (xexpr-path-first `(Subsection (Code ,(format "se=\"~a\",ss=\"~a\"" sec subsec))) content))))
+      (if (empty? (xexpr-path-list '(Subsection) content))
+          (txexpr 'div empty `(,(txexpr 'p empty (list (txexpr 'b empty `(,(format "~a" sec))) " " (xexpr-path-first '(Text) content))) ,@(map parse-paragraph-content (xexpr-path-list '(Paragraph) content))))
+          (txexpr 'div empty (map parse-subsec-specialized (xexpr-path-list '(Subsection) content))))))
+
+(define (parse-subsec-content content parent-section-label)
+  (define label
+    (car (get-elements (xexpr-path-first '(Label) content))))
+  (define text
+    (xexpr-path-first '(Text) content))
+  (define paras (xexpr-path-list '(Paragraph) content))
+  (txexpr 'div '((class "subsec")) `(,(txexpr 'p empty (list (txexpr 'b empty `(,(format "~a ~a" parent-section-label label))) " " text)) ,@(map parse-paragraph-content paras))))
+
+(define (parse-paragraph-content content)
+  (define label
+    (car (get-elements (xexpr-path-first '(Label) content))))
+  (define text
+    (xexpr-path-first '(Text) content))
+  (define subparas (xexpr-path-list '(Subparagraph) content))
+  (txexpr 'div '((class "para")) `(,(txexpr 'p empty (list (txexpr 'b empty `(,(format "~a" label))) " " text)) ,@(map parse-subparagraph-content subparas))))
+
+(define (parse-subparagraph-content content)
+  (define label
+    (car (get-elements (xexpr-path-first '(Label) content))))
+  (define text
+    (xexpr-path-first '(Text) content))
+  (define clauses (xexpr-path-list '(Clause) content))
+  (txexpr 'div '((class "subpara")) `(,(txexpr 'p empty (list (txexpr 'b empty `(,(format "~a" label))) " " text)) ,@(map parse-clause-content clauses))))
+
+(define (parse-clause-content content)
+  (define label
+    (car (get-elements (xexpr-path-first '(Label) content))))
+  (define text
+    (xexpr-path-first '(Text) content))
+  (define subclauses (xexpr-path-list '(Subclause) content))
+  (txexpr 'div '((class "clause")) `(,(txexpr 'p empty (list (txexpr 'b empty `(,(format "~a" label))) " " text)) ,@(map parse-subclause-content subclauses))))
+
+(define (parse-subclause-content content)
+  (define label
+    (car (get-elements (xexpr-path-first '(Label) content))))
+  (define text
+    (xexpr-path-first '(Text) content))
+  (txexpr 'div '((class "subclause")) `(,(txexpr 'p empty (list (txexpr 'b empty `(,(format "~a" label))) " " text)))))
 
 (provide (all-from-out "../pollen.rkt"))
 (provide (all-defined-out))
