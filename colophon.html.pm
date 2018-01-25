94e80c57aa5e5470d2a769a05c280ed5743efbad
Sancho McCann <sanchom@gmail.com>
2018-01-24 21:39:09 -0800

    Added colophon.

---
+++ colophon.html.pm
@@ -0,0 +1,15 @@
+#lang pollen
+
+◊define-meta[page-title]{Colophon}
+
+◊title{Colophon}
+
+The typeface is ◊a[#:href "https://edwardtufte.github.io/et-book/"]{ET Book}.
+
+I write this site using ◊a[#:href "https://www.gnu.org/software/emacs/"]{Emacs} and
+◊a[#:href "https://racket-lang.org/"]{DrRacket} in a custom markup language that is
+then rendered into HTML and CSS using the ◊a[#:href "http://docs.racket-lang.org/pollen/"]{Pollen}
+publishing program. The source for this site is ◊a[#:href "https://github.com/sanchom/sanchom.github.io"]{here}.
+
+I have been heavily influenced by Matthew Butterick's ◊em{◊a[#:href "https://practicaltypography.com/"]{Practical Typography}}
+and I copied many CSS techniques (e.g. underlining, responsive margin notes) from ◊a[#:href "https://edwardtufte.github.io/tufte-css/"]{Tufte CSS}.
