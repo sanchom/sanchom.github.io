38e0b8f3d2cb0cb5c60915fdc57ac64b33b14c89
Sancho McCann <sanchom@gmail.com>
2018-09-04 23:15:29 -0700

    Art as experience.

---
+++ art-as-experience.html.pm
@@ -0,0 +1,77 @@
+#lang pollen
+
+◊define-meta[page-title]{Art as Experience}
+◊define-meta[snippet]{Prose becomes art when the sentence and
+paragraph structures, connectives, and word choices reflect not just
+the logical truth being presented, but also the experience, the
+feeling, that the author is trying to share.}
+◊define-meta[original-date]{2018-09-04}
+
+◊title{Art as Experience}
+
+I've been reading ◊em{Art as Experience}◊note{◊cite-book[#:author
+"John Dewey" #:year "1934" #:title "Art as Experience" #:publisher
+"Minton Balch & Co." #:location "New York"]} a few pages at a time
+over the past year. I've needed to read it like this because of how
+much is in every paragraph and how much it pulls at ideas from
+elsewhere. Here, I want to share a couple of themes that have come up
+throughout the book.
+
+Every medium is unique. When you practice using a medium, you learn
+what choices an artist is forced to make when they use that medium to
+express their ideas. You can more clearly see what aspects of
+experience an artist has chosen to express. What objects,
+characteristics, and relationships has the artist selected and
+emphasized? Those decisions work their way into expression differently
+with every medium.
+
+One common writing exercise is to read a piece of prose, hide it, then
+try to reproduce it.◊note{◊a[#:href
+"https://itchyquill.wordpress.com/2015/03/26/the-art-of-writing-practising-plagiarism/"]{The
+Art of Writing; Practising Plagiarism (Or Rather,
+Copywork)}.}◊note{◊a[#:href
+"https://www.quora.com/Can-copying-novels-help-me-improve-my-writing"]{Can
+copying novels help me improve my writing?}}◊note{◊a[#:href
+"http://www.jenmanuel.com/process-tips/write-best-story-ever-one-epic-exercise/"]{How
+to write the best story ever with one epic exercise}.} But not by
+simply memorizing it word for word. The key to that exercise is to
+imagine a set of decisions behind the text. In doing this, you become
+more aware of what has been emphasized, what has been ignored, what
+relationships were raised to prominence, and how the text was used to
+achieve this.
+
+A second theme distinguishes between media and mere means. Better
+artists can make their chosen medium one with the expression and not
+merely a means.
+
+◊q{conversation, drama, novel, and architectural construction, if
+there is an ordered experience, reach a stage that at once records and
+sums up the value of what precedes, and evokes and prophesies what is
+to come.}
+
+◊q{[n]ot all means are media. There are two kinds of means. One kind
+is external to that which is accomplished; the other kind is taken up
+into the consequences produced and remains immanent in them. There are
+ends which are merely welcome cessations and there are ends that are
+fulfillments of what went before. ◊elide Sometimes we journey to get
+somewhere else because we have business at the latter point and would
+gladly, were it possible, cut the travelling. At other times we
+journey for the delight of moving about and seeing what we see. Means
+and end coalesce. ◊elide all the cases in which means and ends are
+external to one another are non-esthetic. ◊elide In all ranges of
+experience, externality of means defines the mechanical. ◊elide Means
+are, then, media when they are not just preparatory or preliminary.}
+
+This also comes up in Thomas and Turner◊note{◊cite-book[#:author
+"Francis-Noël Thomas and Mark Turner" #:year "2011" #:title "Clear and
+Simple as the Truth: Writing Classic Prose" #:publisher "Princeton
+University Press"]}: how words can be used in a way that they become
+one with the expression. Prose becomes art when the sentence and
+paragraph structures, connectives, and word choices reflect not just
+the logical truth being presented, but also the experience, the
+feeling, that the author is trying to share.
+
+◊q{in [a] classic sentence, the last section is the conclusion of all
+that has gone before it; the beginning of the sentence exists for the
+end, and the sentence is constructed so that we can anticipate
+arriving at such a conclusion.}
