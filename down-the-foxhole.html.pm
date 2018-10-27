e69768bd487c32458e89388d019a3b5912f1857d
Sancho McCann <sanchom@gmail.com>
2018-10-26 18:27:57 -0700

    Added source.

---
+++ down-the-foxhole.html.pm
@@ -0,0 +1,280 @@
+#lang pollen
+
+◊define-meta[page-title]{Down the foxhole}
+◊define-meta[original-date]{2018-10-26}
+◊define-meta[snippet]{Pierson v Post: who owns a killed fox?}
+
+◊title{Down the foxhole}
+
+◊fig[#:src "assets/desportes-fox.jpg" #:width "300px"]
+
+In our Property Law course, we are now reading about possession. Our
+case book mentioned ◊em{Pierson v Post}◊note{◊em{Pierson v Post}
+(1805), 3 Cai R 175 (NY). Find the full text ◊a[#:href
+"http://www.courts.state.ny.us/reporter/archives/pierson_post.htm"]{here}. There's
+a good ◊a[#:href
+"https://en.wikipedia.org/wiki/Pierson_v._Post"]{Wikipedia article}
+too.} in a note, and that led me down a trail of reading this past
+week.
+
+◊sub-heading{The facts}
+
+Lodowick Post was in pursuit of a fox. Another person, Pierson, shot
+the fox and took it for himself. The issue is whether Post has any
+property right in the fox that he was hunting.◊note{These are the
+scant facts as outlined in the case. Much more has been learned about
+this case since then. See Angela Fernandez, “◊a[#:href
+"https://www.jstor.org/stable/27641649"]{The Lost Record of Pierson
+v. Post, the Famous Fox Case},” ◊em{Law and History Review} 27, no. 1
+(2009): 149–78.}
+
+◊sub-heading{The decision}
+
+The court held that "mere pursuit gave Post no legal right to the fox,
+but that he became the property of Pierson, who intercepted and killed
+him." They seem to give two justifications for this approach. The
+first is a ◊em{first-occupancy} or ◊em{first-possession}
+justification:
+
+◊q{the mortal wounding of such beasts, by one not abandoning his
+pursuit, may, with the ◊u{utmost propriety}, be deemed possession of
+him; since, thereby, the pursuer manifests an unequivocal intention of
+appropriating the animal to his individual use, ◊u{has deprived him of
+his natural liberty, and brought him within his certain control}
+(emphasis mine)}
+
+They also give a justification from enforcement efficiency: "for the
+sake of certainty, and preserving peace and order in society". They
+predict that if they were to rule that mere pursuit gives rise to a
+property right, "it would prove a fertile source of quarrels and
+litigation."
+
+The dissent (Judge ◊a[#:href
+"https://en.wikipedia.org/wiki/Henry_Brockholst_Livingston"]{Henry
+Livingstone}) disagrees and gives an argument based on judicial
+deference, labour, economic efficiency, and some interesting
+assumptions about the role of the fox hunt in New York in 1805.
+
+First, the Livingstone says the court should have just asked the
+"sportsmen" (the fox hunters) what would rule would have made sense in
+the context of their activity. ◊note{This contextual approach to
+possession has actually been embraced in the common law. See ◊em{Popov
+v Hayashi}, 2002 WL 31833731 (Cal Super Ct): "These rules are
+contextual in nature. The are crafted in response to the unique nature
+of the conduct they seek to regulate. Moreover, they are influenced by
+the custom and practice of each industry."}
+
+Regarding labour, economic incentives, and the role of the fox hunt,
+he says (emphasis and translations mine):
+
+◊q{ By the pleadings it is admitted that a fox is a "wild and noxious
+beast." Both parties have regarded him, as the law of nations does a
+pirate, “◊em{hostem humani generis},” [enemy of mankind] and although
+“◊em{de mortuis nil nisi bonum},” [of the dead, say nothing but good]
+be a maxim of our profession, the memory of the deceased has not been
+spared. His depredations on farmers and on barn yards, have not been
+forgotten; ◊u{and to put him to death wherever found, is allowed to be
+meritorious, and of public benefit}. Hence it follows, that our
+decision should have in view the greatest possible encouragement to
+the destruction of an animal, so cunning and ruthless in his
+career. But who would keep a pack of hounds; or what gentleman, at the
+sound of the horn, and at peep of day, would mount his steed, and for
+hours together, “◊em{sub jove frigido},” [under cold Jupiter] or a
+vertical sun, pursue the windings of this wily quadruped, if, just as
+night came on, and his stratagems and strength were nearly exhausted,
+a ◊u{saucy intruder}, who had not shared in the ◊u{honours or labours
+of the chase}, were permitted to come in at the death, and bear away
+in triumph the object of pursuit?}
+
+◊sub-heading{Economic efficiency}
+
+Livingstone is arguing that there is social good in killing foxes,
+particularly because of their damage to farm animals. It's important
+to encourage the fox hunt because it kills foxes. If a hunter who
+spent a bunch of effort on pursuing a fox might lose it to a "saucy
+intruder", there will be fewer hunters willing to put in that effort,
+and there will be fewer foxes killed. So, let's not reward that
+intruder with ownership of the fox.
+
+These are not obvious moves. You might think---a lot of people
+think---that judges merely try to discover what the law ◊em{is}. And
+some people think that is a search for something called "natural law":
+law that exists independently of our particular circumstances and
+societies---objective moral truth. Or, slightly less extreme, that
+judges are searching for a rule that is based on long-standing, even
+though socially contingent, ideas about what is ◊em{right} in a moral
+sense. This case makes it clear that judges sometimes ◊em{make} law,
+and they sometimes make law based on pragmatic concerns and economic
+concerns.◊note{Or maybe, they are ◊em{always} maximizing economic
+efficiency. See ◊cite-book[#:author "Richard A Posner" #:title
+"Economic Analysis of Law" #:year "1973" #:publisher "Little, Brown"]
+And, ◊cite-article[#:author "Richard A Posner" #:title "The Economic
+Approach to Law" #:year "1975" #:journal "Texas Law Review" #:volume
+"53" #:pages "757" #:url
+"http://chicagounbound.uchicago.edu/cgi/viewcontent.cgi?article=2881&context=journal_articles"]}
+I don't know how often it was that judges considered these interests
+in 1805 or how explicitly they typically presented those
+justifications in their decisions.
+
+Economic-efficiency arguments are tricky. You have to (at least
+implicitly) choose a "welfare" function: an accounting of things that
+you want to see increase and things that you want to see decreases. A
+paper by Dharmpala and Pitchford actually works through the effect
+that the majority's rule and Livingstone's rule would have on the
+number of foxes killed.◊note{◊cite-article[#:author "Dhammika
+Dharmpala and Rohan Pitchford" #:year "2002" #:volume "18" #:issue "1"
+#:pages "39--66" #:title "An Economic Analysis of Riding to Hounds:
+Pierson v Post Revisited" #:journal "Journal of Law, Economics, &
+Organization" #:url "https://www.jstor.org/stable/pdf/3555069.pdf"]}
+The results under their initial assumptions roughly match
+Livingstone's intuition in his dissent. The paper is fairly readable
+even if you ignore every equation.
+
+Thinking about property allocation from this economic perspective is
+only a tool for expressing reasons; it doesn't provide reasons on its
+own. What you choose to capture in the welfare function can nearly
+answer the question for you. In Dharmpala and Pitchford's article,
+they included the price per pelt, enjoyment of the hunt, damage
+averted to farm animals, investment by the hunter, and investment by
+the saucy intruder. What about other externalities?
+Enforcement/litigation costs might be significantly different under
+each of the rules. In fact, the majority argues this, and Dharmpala
+and Pitchford expand their welfare function to include enforcement
+costs later in the paper. This just highlights how the definition of
+the social welfare function is a political question. And, the
+maximization of it is contingent on actual facts, some of which might
+not actually be known or knowable. Why does the original hunter get
+zero positive enjoyment when the intruder gets the fox?  Even assuming
+that the positive experience during the hunt is diminished when the
+intruder gets the fox, why does it cancel perfectly to zero?
+
+It turns out that hunters found pleasure in the hunt even if they
+didn't end up catching a fox. They also may have sought to maintain
+the fox population. A "bagged" (live) fox was actually worth more than
+a dead fox.◊note{◊cite-article[#:title "Legal Fictions in Pierson v
+Post" #:author "Andrea McDowell" #:year "2007" #:journal "Michigan Law
+Review" #:volume "105" #:issue "4" #:pages "735--778" #:url
+"https://repository.law.umich.edu/cgi/viewcontent.cgi?article=1478&context=mlr"]}
+Again, this demonstrates that economic arguments are only as
+convincing as the factors they consider.
+
+How would the Coase theorem apply here?◊note{◊cite-article[#:url
+"https://www.law.uchicago.edu/files/file/coase-problem.pdf" #:author
+"RH Coase" #:journal "Law and Economics" #:year "1960" #:volume "3"
+#:pages "1--44" #:title "The Problem of Social Cost"]} Perhaps the
+hunters would pay people to stay clear of their hunt. The hunters
+would have to pay the intruders as much money as they'd otherwise be
+able to fetch by freeloading. But this actually seems untenable,
+because more and more people could show up demanding payment to stay
+out of the way of the hunt. Historically, though, it seems that rather
+than the intruders being mere freeloaders, they were actually farmers
+just trying to ensure the foxes got killed, to prevent injury to their
+own animals.◊note{McDowell, ◊em{supra} note 6.} If the only people
+that were intruding were farmers at risk of losing animals to foxes,
+then a Coasian solution could actually play out. The hunters would pay
+the farmers for damage that happens to come to their farms due to
+foxes: a kind of tax/insurance for having a pure
+hunt.◊note{◊em{Ibid.}} Another option would be payments to the
+owner(s) of the land on which the hunt takes place in exchange for the
+exclusive right to hunt. (Although, the fox in ◊em{Pierson v Post} was
+killed on a public beach.)
+
+◊sub-heading{Application: cybersquatting}
+
+Where does this kind of situation occur today? The situation,
+abstractly, is that there are two groups of people seeking benefit
+from a limited resource. The resource is such that it takes some
+effort to extract value from that resource and the person to initiate
+that effort doesn't necessarily gain exclusive possession of that
+value for quite some time, leaving room for a "saucy intruder" to cut
+in and freeload or hold hostage the value desired by the person who
+undertook the initial investment of effort.
+
+Dharmpala and Pitchford argue that ◊a[#:href
+"https://en.wikipedia.org/wiki/Cybersquatting"]{cybersquatting}
+matches this scenario.◊note{Dharmpala and Pitchford, ◊em{supra}, note
+5.} Should a domain name be given to the first person to register it
+without regard to whether another has already developed a brand around
+that name? Or should a person who has put in the "honour and labour"
+of developing a company and brand have rights to the associated domain
+name? It turns out that in cases of pure cybersquatting (as opposed to
+dualing brands), the common law, and now legislation, has applied a rule
+analogous to Livingstone's rule: the company with the developed brand
+gets the domain, rather than the cybersquatter.
+
+◊sub-heading{Application: patent law}
+
+This analysis has also been applied to criticize some outcomes of
+patent law.◊note{◊cite-article[#:author "Lara Larrimore Oullette"
+#:title "Pierson, Peer Review, and Patent Law" #:journal "Vanderbilt
+Law Review" #:volume "69" #:pages "1825" #:url
+"https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2803479" #:year
+"2016"]}
+
+Patent law can result in a division of rights between an initial
+inventor and subsequent inventors that improve upon the original. An
+inventor can only protect, exclude, and exploit that which they've
+actually invented. Improvements are patentable separately by
+subsequent inventors. But a subsequent inventor having improved upon a
+previous patent could not sell the improved variant without a licence
+from the holder of the original patent when that improvement builds
+upon the original.◊note{◊cite-article[#:title "The Relationship
+Between Basic Patents and Improvement Patents" #:author "Arnold
+Silverman" #:journal "JOM" #:volume "47" #:issue "1" #:year "1995"
+#:url
+"https://www.tms.org/pubs/journals/JOM/matters/matters-9501.html"
+#:pages "50"]}◊note{◊cite-article[#:url
+"https://digitalcommons.law.scu.edu/cgi/viewcontent.cgi?article=1124&context=chtlj"
+#:title "Patenting Improvements: The Costs of Making Patents Easily
+Available" #:author "Douglas Applegate" #:volume "8" #:issue "2"
+#:pages "429--446" #:journal "Santa Clara High Tech LJ" #:year
+"1992"]} And, the original inventor would need to get a licence from
+the improver in order to use their improvement.
+
+The current law rewards early investment. We give patents to
+inventions that might not be market-ready. Patent law does not require
+commercial viability. However, patent law also rewards subsequent
+investment by other inventors. It might take less investment to take
+the invention from its earliest-patentable state to a
+commercially-viable state. Improvers can ride on the early success of
+the original patent-holder, but they can't free-ride. The improver
+can't actually sell the commercially-viable product if it still uses
+the original invention. However, the improver could exclude the
+original inventor from using the particular improvement that is
+patented by the improver. This could be used to extract a licensing
+fee from the original inventor in order to make, use, or sell the
+improver's commercially-viable variant. Here, we see a
+mixed-allocation of rights in action.
+
+But, what if we allocate rights too early? What if the original
+inventor gets rights while they are still in "mere pursuit" of the
+invention? As per Livingstone's analysis, this will discourage the
+saucy intruder. Here, though, the saucy intruder is a would-be
+inventor, a would-be improver. We don't actually want to discourage
+this activity. That would actually work against the goals of the patent
+system.
+
+Oullette argues that this happens when we give people patents that are
+too speculative and that require undue experimentation on the part of
+anyone wanting to use the patent. These patents don't actually do much
+to lower the barrier to further improvement, but they reward the
+original "inventor" with an unearned right to exclude. This actually
+does discourage the flurry of improvement patents that one might
+normally see after a sufficiently disclosed invention is
+patented.◊note{Oullette, ◊em{supra} note 11.} She argues that this
+is a defect in the examination process.
+
+◊sub-heading{How people use this case}
+
+I found an article by Luke Meier on ◊a[#:href
+"https://papers.ssrn.com/sol3/papers.cfm?abstract_id=2824501"]{using
+◊em{Pierson v Post} as the first case in property law}. This is
+almost a transcript of how he would teach his first few classes.
+
+It seems that it's common to use this as the first case in a property
+law course. We didn't look at this case first. We actually started by
+thinking about what rights are part of the bundle of rights called
+property, how those are contextual, and the different roles that
+property rights fill in our society. Only now, seven weeks in, are we
+starting to look at what is required, or what should be required, to
+gain "possession" of a thing.
