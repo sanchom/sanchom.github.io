7af8abf9a1a298e885b6fe178b07ef7518908492
Sancho McCann <sanchom@gmail.com>
2019-05-16 20:31:17 -0700

    Updated front page.

--- index.html.pm
+++ index.html.pm
@@ -2,14 +2,14 @@
 
 ◊define-meta[page-title]{Home}
 
-I'm in my first year at UBC's Allard School of Law (class of 2021). I
-want use law to help give the weakest of us a voice, in the justice
-system and in society in general. This includes civil liberties,
-especially free expression and defendants' rights. I want to help with
-public legal education and access-to-justice initiatives, to give more
-of us more confidence in the outcomes of the justice system and other
-government decision-making processes. I'm also interested in copyright
-law.
+In September, I'll be entering second year at UBC's Allard School of
+Law (class of 2021). I want use law to help give the weakest of us a
+voice, in the legal system and in society in general. This includes
+civil liberties, especially free expression and defendants' rights. I
+want to help with public legal education and access-to-justice
+initiatives, to give more of us more confidence in the outcomes of the
+legal system and other government decision-making processes. I'm also
+interested in the many overlaps of law and technology.
 
 I'm trained as a computer scientist. In that field, I've focused on
 visual object recognition, machine learning, and applications of deep
@@ -17,8 +17,8 @@ neural networks.
 
 Formerly: data scientist at Kobo, head of R&D at Shelfie,
 communications-and-development volunteer at the ◊a[#:href
-"http://bccla.org/"]{BCCLA}, class 1 flight instructor, Canadian
-Forces officer (Cadet Instructor Cadre), 2× Google intern.
+"http://bccla.org/"]{BCCLA}, flight instructor, Canadian Forces
+officer (Cadet Instructor Cadre), 2× Google intern.
 
 If you'd like to get updates from me whenever I write something,
 ◊outline{◊a[#:href "https://tinyletter.com/sanchom"]{sign up for my tinyletter
