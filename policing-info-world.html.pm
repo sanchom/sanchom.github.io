736b063099fafd26e4819bdfac3976b5bdb99c88
Sancho McCann <sanchom@gmail.com>
2019-05-26 11:01:01 -0700

    Copyedit to policing info world.

--- policing-info-world.html.pm
+++ policing-info-world.html.pm
@@ -3,14 +3,15 @@
 ◊define-meta[page-title]{Policing Info World}
 ◊define-meta[snippet]{A review of a conference on data, law enforcement, and surveillance.}
 ◊define-meta[original-date]{2019-05-25}
+◊define-meta[edited-date]{2019-05-26}
 
 ◊title{Policing Info World: Conference Review}
 
 On May 23, I attended a free, one-day conference on data, law
-enforcement, and surveillance that was hosted by the ◊a[#:href
-"https://fipa.bc.ca"]{BC Freedom of Information and Privacy
-Association} (BC FIPA), the ◊a[#:href "https://bccla.org"]{BC Civil
-Liberties Association} (BCCLA), and ◊a[#:href
+enforcement, and surveillance: "Policing Info World". It was hosted by
+the ◊a[#:href "https://fipa.bc.ca"]{BC Freedom of Information and
+Privacy Association} (BC FIPA), the ◊a[#:href "https://bccla.org"]{BC
+Civil Liberties Association} (BCCLA), and ◊a[#:href
 "https://www.kpu.ca"]{Kwantlen Polytechnic University} (KPU).
 
 There were four panels: one on surveillance, one on predictive and
@@ -33,35 +34,35 @@ Josh Patterson, Executive Director of the BCCLA.
 
 }
 
-On this panel, Michelle Davey discussed the VPD's plans for a
+On this panel, Michelle Davey presented the VPD's plans for a
 drone/uncrewed aerial vehicle. They are still developing the policy
-that will control its use, but she was able to tell us that it will
-primarily be used for emergency response and gathering data to analyze
-an accident or crime that has already happened, especially vehicle
+that will control its use, but she told us that it will primarily be
+used for emergency response and for gathering data to analyze an
+accident or crime that has already happened, especially vehicle
 collisions. She said that it would be also used during large public
 events to monitor unlawful gatherings of people (mentioning the 2011
 Stanley Cup Riots as an example where this would have been
 useful). Each use of the drone will require high-level approval and
 they will document and publish each use of the drone.
 
-Josh Patterson expressed support for all the uses of the drone that
-were mentioned, with the exception of potentially indiscriminate
+Josh Patterson expressed support for most uses of the drone that Davey
+mentioned, with the exception of the potentially indiscriminate
 wide-area surveillance at public events. When people expect to be
 surveilled, they may be less likely to show up at an event they would
 have otherwise been present for, even when their presence would have
 been wholly peaceful and lawful.
 
-I hope that that VPD's drone policy discloses the factors that they
-will use to determine whether a public event justifies the use of a
-drone for pre-emptive monitoring and what is done with the data in the
-case that no criminal investigations follow from the event.
+I hope that that VPD's drone policy will disclose the factors that
+they will use to determine whether a public event justifies the use of
+a drone for pre-emptive monitoring and what is done with the data in
+the case that no criminal investigations follow from the event.
 
 The VPD also already operates a helicopter. While the use of a drone
-may bring certain privacy issues to the forefront and will potentially
-subject more people more frequently to surveillance, it is not
-different in kind from that which is already possible.
+may bring certain privacy issues to the forefront and may subject more
+people more frequently to surveillance, it is not different in kind
+from that which is already possible.
 
-◊sub-heading{Predictive policing}
+◊sub-heading{On predictive policing}
 
 The panel:
 
@@ -75,19 +76,18 @@ information.
 }
 
 Ryan Prox presented how the VPD uses predictive policing to inform
-where they should place officers. They restrict the use of predictive
-policing to only the most patterned and predictable crime: property
-offenses. You can see the data that they use as input to the system at
-◊a[#:href "https://geodash.vpd.ca/"]{geodash.vpd.ca}.
+where they should place officers. They use predictive policing only
+for the most patterned and predictable crime: property offenses. You
+can see the data that they use as input to the system at ◊a[#:href
+"https://geodash.vpd.ca/"]{geodash.vpd.ca}.
 
 Of course, police presence itself would increase the observations of
 crime in a particular area, thus potentially creating a feedback cycle
 where the system would suggest deployments to areas where crime had
 been observed by officers, because that's where they had been deployed
-in the past. However, the system does not include property crimes that
-were reported by an officer: it only bases its predictions on property
-crime that is reported by the property owner, thus avoiding the
-particular feedback loop just mentioned.
+in the past. To avoid this, the system does not include property crime
+that was reported by an officer; it only bases its predictions on
+property crime that is reported by the property owner.
 
 The system also isn't used for predictive deployments to the downtown
 eastside or in areas around temporary modular housing. These areas
@@ -95,20 +95,20 @@ already have their own community-policing programs and officers that
 are specially trained to work in these areas and with these
 communities.
 
-It did seem like this system is being developed with an awareness of
-the risks inherent in off-loading decision-making to a
-mathematical/algorithmic process. Prox pointed to the Federal Treasury
-Board's ◊a[#:href
+There are risks inherent in off-loading our decision-making to a
+mathematical/algorithmic process. It seemed like the VPD is aware of
+this and approaching the issue thoughtfully. Prox pointed to the
+Federal Treasury Board's ◊a[#:href
 "https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=32592"]{Algorithmic
 Impact Assessment} which, while not legally applicable outside of the
 federal government, gives a useful framework for managing the human
 risks of algorithmic decision-making in general. For example, if an
 automated decision will "likely have very high impacts on ◊elide the
 rights of individuals or communities", then the government must (among
-other things) allow peer review by non-government, publish
-descriptions of the system and training data, have the final decision
-be made by a human, and conduct ongoing monitoring to detect
-unintentional outcomes.
+other things) obtain peer review by non-government, publish
+descriptions of the system and training data, require the final
+decision to be made by a human, and conduct ongoing monitoring to
+detect unintentional outcomes.
 
 ◊sub-heading{On the ideal of bias-free policing}
 
@@ -149,7 +149,7 @@ call for data on criminal victimization and to "monitor and evaluate
 progress" in eliminating over-representation of Indigenous people in
 custody, for example.
 
-◊sub-heading{On data searches at the border}
+◊sub-heading{On searches for data when crossing the border}
 
 The panel:
 
@@ -171,12 +171,12 @@ customs- or immigration-related offences.
 #:year "1985" #:chapter "1 (2nd Supp)" #:id "Customs Act" #:url
 "https://laws-lois.justice.gc.ca/eng/acts/c-52.6/FullText.html"]
 
-There is very little case law on the issue, but what is sure is that
-CBSA requires no reasonable grounds (as would normally be required for
-a search) to search your goods at the border.◊note-cite["Customs Act"
-#:pinpoint "Section 99(1)(a)"] And "goods, for greater certainty,
-includes conveyances, animals and any document in any
-form."◊note-cite["Customs Act" #:pinpoint "Section 2(1)"]
+It is clear that CBSA requires no reasonable grounds (as would
+normally be required for a search) to search your goods at the
+border.◊note-cite["Customs Act" #:pinpoint "Section 99(1)(a)"] And
+"goods, for greater certainty, includes conveyances, animals and any
+document in any form."◊note-cite["Customs Act" #:pinpoint "Section
+2(1)"]
 
 ◊declare-work[#:type "legal-case" #:title "R v Gibson" #:citation
 "2017 BCPC 237" #:url
@@ -188,20 +188,22 @@ ONSC 5642" #:url
 "https://www.canlii.org/en/on/onsc/doc/2012/2012onsc5642/2012onsc5642.html"
 #:id "Moroz"]
 
-What is less clear is how the government is to treat the data that is
-on your phone. Is it a good? All we have today are some lower court
-decisions that say the data is a "good" for the purposes of the
+Less clear is how the government is to treat the data that is on your
+phone. Is it a 'good'? All we have today are some lower court
+decisions that say the data is a 'good' for the purposes of the
 ◊em{Customs Act}.◊note{◊see-eg["Gibson" #:pinpoint "paras 95--98"]
 ◊see-also["Moroz" #:pinpoint "paras 20--21"]}
 
-Edelmann emphasized the discrepancy between how Canada treats data
-that crosses the border through the ordinary means (over the internet)
-and data that happens to cross the border on a physical device like a
-cell phone. He argued the government needs to clarify with legislation
-how ◊em{data} should be regulated and then do that uniformly,
-predictably, and transparently, rather than shoehorning our new
-devices into old categories of searches as a way of getting at a tiny
-fraction of data that is crossing the border.
+Edelmann noted that Canada doesn't have a consistent approach to data
+that crosses the border. Data can cross the border over the internet
+and data can cross the border on a cell phone. The data carried over
+the border on a cell phone is minuscule compared to the data crossing
+over the internet. But the government treats these two streams very
+differently. He argued that the government needs to clarify with
+legislation how ◊em{data} should be regulated and then do that
+uniformly, predictably, and transparently, rather than shoehorning our
+new electronic devices into old categories of searches as a way of
+getting at only a tiny fraction of data that is crossing the border.
 
 Meghan McDermott presented portions of the ◊a[#:href
 "https://bccla.org/wp-content/uploads/2018/07/Electronic-Devices-Privacy-Handbook-BCCLA_2018.pdf"]{Electronic
@@ -210,9 +212,8 @@ published by the BCCLA and the Canadian Internet Policy and Public
 Interest Clinic (CIPPIC) in 2018.
 
 The main takeaway for me was the lack of transparency from the CBSA
-regarding their border-search practices, especially regarding the
-National Targetting Program. This is part of a strategy of pushing our
-borders outward. What might increase the likelihood that your
+about their border-search practices, especially regarding the National
+Targetting Program. What might increase the likelihood that your
 electronic device is searched at the border? It is impossible to know,
 but court cases and access-to-information requests provide some
 hints. The BCCLA/CIPPIC handbook identifies a list of potential
