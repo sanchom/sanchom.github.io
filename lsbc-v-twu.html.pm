be38507301bcd11767c2921107b56f78715a6eb4
Sancho McCann <sanchom@gmail.com>
2019-06-20 07:25:53 -0700

    Case comment.

---
+++ lsbc-v-twu.html.pm
@@ -0,0 +1,153 @@
+#lang pollen
+
+◊define-meta[page-title]{Finding Harmony: Law Society of British Columbia v Trinity Western University}
+◊define-meta[short-title]{Case comment: LSBC v TWU}
+◊define-meta[snippet]{Some background for non-law readers of my case comment on LSBC v TWU.}
+◊define-meta[original-date]{2019-06-20}
+
+◊declare-work[#:type "article" #:author-given "Sancho" #:author-family
+"McCann" #:title "Finding Harmony: *Law Society of British Columbia v
+Trinity Western University*" #:comment-info "Case Comment" #:journal
+"Dalhousie Journal of Legal Studies" #:volume "28" #:forthcoming
+"2019" #:id "McCann" #:short-form "McCann"]
+
+◊declare-work[#:type "legal-case" #:title "Law Society of British
+Columbia v Trinity Western University" #:citation "2018 SCC 32"
+#:short-form "*LSBC v TWU*" #:id "LSBC" #:url
+"https://scc-csc.lexum.com/scc-csc/scc-csc/en/item/17140/index.do"]
+
+◊title{Finding Harmony: ◊em{Law Society of British Columbia v Trinity
+Western University}}
+
+◊subtitle{Sancho McCann}
+
+My case comment is going to be published!◊note{◊cite["McCann"]
+Commenting on ◊cite["LSBC"]} You'll be able to find it in the next
+volume of the ◊a[#:href
+"https://ojs.library.dal.ca/djls"]{◊em{Dalhousie Journal of Legal
+Studies}}. Until then, they've said that I can post this, which should
+be very close to the final draft:
+
+◊center{◊a[#:href "assets/McCann-Finding-Harmony-LSBC-v-TWU.pdf"]{Finding Harmony:
+◊em{Law Society of British Columbia v Trinity Western University}}.}
+
+Here, I want to give some acknowledgements and also set out some
+background for non-law readers.
+
+◊heading{Acknowledgements}
+
+I'd like to thank Professor Margot Young for encouraging me to develop
+these ideas into an article after a discussion during office hours,
+and James Barth, a friend from 1L who read through an early
+draft. Much of the content has changed since those early stages; any
+errors are my own. I'd also like to thank the student editors at the
+Dalhousie Journal of Legal studies. They made this a better paper.
+
+◊heading{Background}
+
+This case is about a discretionary decision made by the Benchers of
+the Law Society of British Columbia. They decided to not approve a law
+school that was proposed by Trinity Western University (TWU). This was
+because of a covenant (a promise) that TWU required students to sign
+which, among other things, "calls on students to abstain from
+sexual intimacy outside of opposite sex marriage."
+
+The decision to not approve the law school was contentious. The
+[British Columbia Civil Liberties
+Association](https://bccla.org/2018/02/twu_message_2018/) described
+the tension: "Board members and staff have struggled with the question
+of whether TWU, a private religious university, should have an
+accredited law school in light of TWU’s mandatory community covenant
+that places burdens and limitations on LGBTQI+ persons that are not
+placed on heterosexual persons."
+
+Because the Law Society of British Columbia is created by statute (the
+◊em{Legal Profession Act}) and is acting with delegated authority, its
+decisions are treated as government action and they must comply with
+the ◊em{Canadian Charter of Rights and Freedoms} (the ◊em{Charter}).
+
+My case comment focuses on how courts review discretionary decisions
+of the Law Society (or discretionary decisions of any administrative
+agency) for compliance with the ◊em{Charter}. This is an important
+question: when a government agency infringes a person's ◊em{Charter}
+right, justified by having to balance that right against the agency's
+statutorily-mandated interests, how should a court decide whether the
+government got that decision, that balance, correct? How strictly
+should our courts review our government's decision-making?
+
+To be clear about who the actors are here:
+
+◊itemize{
+
+◊b{Trinity Western University} is a non-governmental entity that
+proposed a law school while requiring all students to sign the
+community covenant.
+
+The ◊b{Law Society of British Columbia} is an administrative body,
+created by statute, comprising Benchers and Members, which regulates
+many aspects of the legal profession in British Columbia and that is
+considered to be part of government for ◊em{Charter} purposes. It was
+the Law Society who denied TWU the approval for their law school.
+
+◊b{The courts} (the BC Supreme Court, the BC Court of Appeal, and the
+Supreme Court of Canada) had to determine whether the decision of the
+LSBC Benchers to not approve a law school was an impermissible
+infringement of a ◊em{Charter} right.
+
+}
+
+◊declare-work[#:id "dore" #:type "legal-case" #:title "Doré v Barreau
+du Québec" #:citation "2012 SCC 12" #:short-form "Doré" #:url
+"https://scc-csc.lexum.com/scc-csc/scc-csc/en/item/7998/index.do"]
+
+◊declare-work[#:id "loyola" #:type "legal-case" #:title "Loyola High
+School v Quebec (Attorney General)" #:citation "2015 SCC 12"
+#:short-form "Loyola" #:url
+"https://scc-csc.lexum.com/scc-csc/scc-csc/en/item/14703/index.do"]
+
+◊declare-work[#:id "bedford" #:type "legal-case" #:title "Canada
+(Attorney General) v Bedford" #:citation "2013 SCC 72" #:short-form
+"Bedford" #:url
+"https://scc-csc.lexum.com/scc-csc/scc-csc/en/item/13389/index.do"]
+
+Again, this case comment focuses on what the Supreme Court's decision
+in ◊em{LSBC v TWU} tells us about how courts should review the
+discretionary decisions of administrative decision-makers for
+compliance with the ◊em{Charter}. There is a recent line of cases from
+the Supreme Court of Canada that presumably had answered this question
+already.◊note{◊see{dore} ◊see-also{loyola}} The Court generally will
+follow the rules and methods they've set out in previous cases unless
+they prove to be unworkable or if new evidence arises that shows the
+existing precedent was based on flawed or no-longer-valid
+premises.◊note{◊see["bedford" #:pinpoint "para 42" #:parenthetical
+"\"the matter may be revisited if new legal issues are raised as a
+consequence of significant developments in the law, or if there is a
+change in the circumstances or evidence that fundamentally shifts the
+parameters of the debate\""]}
+
+I argue that this dispute between the Law Society and Trinity Western
+University shows that the framework from ◊em{Doré} and ◊em{Loyola} is
+not workable, or at least, that it is not doing what the Court says it
+is doing. I argue that the Court should be reluctant to use the
+framework of deference and reasonableness set out in ◊em{Doré} and
+◊em{Loyola} when the decision-maker (the Law Society of British
+Columbia) fails to produce explicit written reasons for their
+decision. I also argue, though, that the Court ◊em{isn't} actually
+deferring in situations like this (despite the language that they use)
+and that the Court obscures the level of scrutiny that it applies to
+discretionary decisions of agencies like the Law Society of British
+Columbia.
+
+Also, a bit of background about the Supreme Court of Canada decision
+itself. This is a nine-member court. In this case, all nine justices
+participated. The majority (the decision that is considered
+controlling for future cases) was made up of five justices. Two more
+justices concurred, each writing individually (they agreed in the
+result---that the Law Society was right to not approve the law
+school---but gave different reasoning). Two justices dissented,
+writing together (they disagreed with the result---they thought the
+Law Society was wrong in their decision to not approve the law
+school).
+
+I hope this background puts more of a concrete story in your head for
+when you read through the case comment.
