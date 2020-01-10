1417e706def4b4e3380c121d95424dea54ad6168
Sancho McCann <sanchom@gmail.com>
2020-01-09 22:05:13 -0800

    Vavilov and Pastion.

---
+++ vavilov-and-pastion.html.pm
@@ -0,0 +1,83 @@
+#lang pollen
+
+◊define-meta[page-title]{Vavilov and Indigenous self-government}
+◊define-meta[short-title]{Vavilov and Pastion}
+◊define-meta[original-date]{2020-01-20}
+◊define-meta[snippet]{How Vavilov does not undercut Indigenous self-government}
+
+◊declare-work[#:id "Vavilov" #:type "legal-case" #:title "Canada
+(Minister of Citizenship and Immigration) v. Vavilov" #:citation "2019
+SCC 65" #:short-form "*Vavilov*" #:url
+"https://scc-csc.lexum.com/scc-csc/scc-csc/en/item/18078/index.do"]
+
+◊title{◊em{Vavilov} and Indigenous self-government}
+◊subtitle{Sancho McCann}
+
+◊declare-work[#:id "Pastion" #:type "legal-case" #:title "Pastion v
+Dene Tha' First Nation" #:citation "2018 FC 648" #:url
+"https://www.canlii.org/en/ca/fct/doc/2018/2018fc648/2018fc648.html"
+#:short-form "Pastion"]
+
+There has been optimism that ◊em{Pastion v Dene Tha' First
+Nation}◊note-cite["Pastion"] makes space for Indigenous
+self-government within Canada's administrative
+state. Post-◊em{Vavilov}, courts will be reviewing the reasons of
+decision-makers with heightened scrutiny. Could this cut into the
+sphere of deference carved out by ◊em{Pastion} for Indigenous
+decision-makers? I think it does not.
+
+The Court in ◊em{Vavilov} lists various factors that inform the
+scope◊note{Although, not literally a ◊em{range}.} of what is
+reasonable: the statutory scheme, common law, international law,
+evidence, submissions, past practice, etc. While ◊em{Pastion}
+recognized a mandate to give "deference towards Indigenous
+decision-makers tasked with applying Indigenous laws," Indigenous law
+is not mentioned by the Court at all in ◊em{Vavilov}. But I think
+◊em{Vavilov} leaves the holding in ◊em{Pastion} untouched.
+
+First, the Court did not claim to be presenting an exhaustive list of
+factors◊note[#:expanded "yes"]{◊cite["Vavilov" #:pinpoint "para 106"
+#:parenthetical "\"[i]t is unnecessary to catalogue all of the legal
+or factual considerations that could constrain an administrative
+decision maker in a particular case\""]} and emphasized that "each
+decision must be [justified and evaluated] in relation to its own
+particular context."◊note-cite["Vavilov" #:pinpoint "para 90"]
+
+Second, courts are also not to assume that they are the target
+audience of the reasons. "[T]he decision must... be justified... to
+those to whom the decision applies... [E]xercise of public power must
+be justified, intelligible and transparent, not in the abstract, but
+to the individuals subject to it."◊note-cite["Vavilov" #:pinpoint
+"paras 86, 95"]
+
+And last, paragraph 93 of ◊em{Vavilov} echos paragraph 22 of
+◊em{Pastion}:
+
+◊q{ An administrative decision maker may demonstrate through its
+reasons that a given decision was made by bringing that institutional
+expertise and experience to bear. In conducting reasonableness review,
+judges should be attentive to the application by decision makers of
+specialized knowledge, as demonstrated by their reasons. Respectful
+attention to a decision maker’s demonstrated expertise may reveal to a
+reviewing court that an outcome that might be puzzling or
+counterintuitive on its face nevertheless accords with the purposes
+and practical realities of the relevant administrative regime and
+represents a reasonable approach given the consequences and the
+operational impact of the decision. This demonstrated experience and
+expertise may also explain why a given issue is treated in less
+detail. (◊em{Vavilov} at para 93.)}
+
+◊q{ Many forms of knowledge may be grouped under the heading of
+"expertise." Indigenous decision-makers are obviously in a better
+position than non-Indigenous courts to understand Indigenous legal
+traditions. They are particularly well-placed to understand the
+purposes that Indigenous laws pursue. They are also sensitive to
+Indigenous experience generally and to the conditions of the
+particular nation or community involved in the decision. They may be
+able to take judicial notice of facts that are obvious and
+indisputable to the members of that particular community or nation,
+which this Court may be unaware of. Indeed, for many Indigenous
+peoples, a person is best placed to make a decision if that person has
+close knowledge of the situation at issue. (◊em{Pastion} at para 22.)
+}
+
