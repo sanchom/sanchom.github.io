5899d3be068b2b888e1d7a88ec0001f11ebf9680
Sancho McCann <sanchom@gmail.com>
2020-08-30 20:36:30 -0700

    Added some lists.

---
+++ lists.html.pm
@@ -0,0 +1,128 @@
+#lang pollen
+
+◊define-meta[page-title]{Lists}
+
+Here are some of the things I like and that I tend to recommend to
+people.◊note{Inspired by [Lex Gill's
+lists](https://lexgill.com/lists/).}
+
+◊sub-heading{Podcasts}
+
+◊itemize{
+
+[Artificial Intelligence](https://lexfridman.com/podcast/)
+(Conversations with people working on deep learning, AI, cognitive
+science, physics, math, etc.)
+
+[Borderlines](https://borderlines.ca/) (Canadian immigration law and
+policy)
+
+[The Docket](http://www.michaelspratt.com/poadcast-legal-matters)
+(Canadian law, often criminal law)
+
+[Electon Profit Makers](http://www.electionprofitmakers.com/) (I will
+not try to describe this work of art)
+
+[Intrepid](https://www.intrepidpodcast.com/podcast) (Canadian national
+security law and policy)
+
+[Ipse Dixit](https://shows.acast.com/ipse-dixit) (Legal scholarship;
+interviews with authors about their writing)
+
+[Judge John
+Hodgman](https://maximumfun.org/podcasts/judge-john-hodgman/)
+(Humanism, comedy, whether a hot dog is a sandwich)
+
+[Just Between
+Us](https://www.stitcher.com/podcast/stitcher/just-between-us-stitcher)
+(Alison Raskin and Gaby Dunn chat, conduct interviews, and answer
+questions)
+
+[The Lawfare
+Podcast](https://www.lawfareblog.com/topic/lawfare-podcast) (US
+national security law and policy)
+
+[On the Media](https://www.wnycstudios.org/podcasts/otm) (American
+weekly radio show from WNYC, covering anything touching journalism,
+tech, and expression)
+
+[Oral Argument](https://oralargument.org/) (Legal theory, writing,
+jurisprudence, etc.)
+
+[Politicoast](https://www.politicoast.ca/) (BC and Canadian politics)
+
+[Strict Scrutiny](https://strictscrutinypodcast.com/) (US Supreme
+Court)
+
+[The Trauma-Informed
+Lawyer](https://thetraumainformedlawyer.simplecast.com/) ("a
+do-no-further-harm, relational approach to the practice of law";
+"education about trauma, vicarious trauma and trauma-informed
+lawyering")
+
+}
+
+◊sub-heading{New music}
+
+Where I like to find new music.
+
+◊itemize{
+
+[NTS Radio](https://nts.live)
+
+[PyroRadio](http://www.pyroradio.com/)
+
+[RinseFM](https://rinse.fm/)
+
+}
+
+◊sub-heading{Books}
+
+Find me on
+[Goodreads](https://www.goodreads.com/user/show/83412315-sancho).
+
+◊sub-heading{Writing resources}
+
+Just some things that have influenced how I communicate.
+
+◊itemize{
+
+Bryan A. Garner, ◊a[#:href
+"https://global.oup.com/academic/product/garners-modern-english-usage-9780190491482"]{◊em{Garner's
+Modern English Usage}}, 4th ed (New York: Oxford University Press,
+2016).
+
+Bryan A. Garner & David Foster Wallace, ◊em{Quack this Way: David
+Foster Wallace & Bryan A. Garner Talk Language and Writing} (Dallas:
+RosePen Books, 2013).
+
+Interview of Toni Morrison by Elissa Schappell & Claudia Brodsky
+Lacour in “[Tony Morrison, The Art of Fiction
+No. 134](https://www.theparisreview.org/interviews/1888/the-art-of-fiction-no-134-toni-morrison)”,
+◊em{The Paris Review} (1993).
+
+Steven Pinker, ◊a[#:href
+"https://stevenpinker.com/publications/sense-style-thinking-persons-guide-writing-21st-century"]{◊em{The
+Sense of Style: The Thinking Person's Guide to Writing in the 21st
+Century}} (New York: Penguin, 2014).
+
+Francis-Noël Thomas & Mark Turner, ◊a[#:href
+"http://classicprose.com/"]{◊em{Clear and Simple as the Truth: Writing
+Classic Prose}} (Princeton: Princeton University Press, 2011)
+
+Edward Tufte, ◊a[#:href
+"https://www.edwardtufte.com/tufte/books_vdqi"]{◊em{Visual Display of
+Quantitative Information}}, 2nd ed, (Cheshire, CT: Graphics Press,
+2001).
+
+Chelsea Vowel, ◊a[#:href
+"https://www.portageandmainpress.com/product/indigenous-writes/"]{◊em{Indigenous
+Writes: A Guide to First Nations, Métis & Inuit Issues in Canada}}
+(Winnipeg: Highwater Press, 2016).
+
+David Foster Wallace, “[Tense Present: Democracy, English, and the
+Wars over
+Usage](https://harpers.org/wp-content/uploads/HarpersMagazine-2001-04-0070913.pdf)”,
+◊em{Harpers} (April 2001).
+
+}
