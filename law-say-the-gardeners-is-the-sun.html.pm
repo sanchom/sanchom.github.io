9c8ec6bb26610bcea9e3b0f2c0536aa04da3b337
Sancho McCann <sanchom@gmail.com>
2021-01-25 09:01:09 -0800

    Auden

---
+++ law-say-the-gardeners-is-the-sun.html.pm
@@ -0,0 +1,98 @@
+#lang pollen
+
+◊define-meta[page-title]{Auden, "Law, say the gardeners, is the sun"}
+◊define-meta[short-title]{WH Auden: Law is...}
+◊define-meta[original-date]{2021-01-24}
+◊define-meta[featured-image-url]{assets/auden.png}
+
+◊declare-work[#:type "article" #:id "Hussain" #:author "Nasser
+Hussain" #:title "Auden's Law Like Love" #:journal "Law, Culture and
+the Humanities" #:year "2019" #:url
+"https://journals.sagepub.com/doi/abs/10.1177/1743872118787567?journalCode=lcha"]
+
+This poem by WH Auden is a succinct presentation of many of the
+conceptions of law we encountered in our Jurisprudence course.
+
+You can read the full poem
+[here](https://allpoetry.com/Law,-Like-Love).◊note{That site, like
+many others, gives it the title, "Law, Like Love," but it was
+published without a title and anthologized by its first line "Law, say
+the gardeners, is the sun." ◊cite["Hussain"]}
+
+It opens with a reference to law and obedience that are based in
+prediction and perhaps natural law.
+
+◊codeblock{
+Law, say the gardeners, is the sun,
+Law is the one
+All gardeners obey
+To-morrow, yesterday, to-day.
+}
+
+This predictive focus is also found in the writing of Kelsen and
+Holmes Jr.
+
+The next stanza presents a tension between tradition --- couched in
+wisdom --- and sensitivity to new circumstances.
+
+◊codeblock{
+Law is the wisdom of the old,
+The impotent grandfathers feebly scold;
+The grandchildren put out a treble tongue,
+Law is the senses of the young.
+}
+
+This tension is also expressed by Oliver Wendell Holmes, Jr when he
+compares tradition to a dragon that we must decide to either "kill" or
+"tame" and make a "useful animal."
+
+The poem presents HLA Hart's separation of law and morals
+(◊em{Yet law-abiding scholars write: / Law is neither wrong nor
+right}) and notions of notice and prospectivity (◊em{Law is as I've
+told you before, Law is as you know I suppose}).
+
+◊declare-work[#:id "Cover" #:type "article" #:author-given "Robert M"
+#:author-family "Cover" #:title "Forward: *Nomos* and Narrative"
+#:journal "Harv L Rev" #:volume "97" #:issue "4" #:year "1983--1984"
+#:first-page "4" #:url
+"https://digitalcommons.law.yale.edu/cgi/viewcontent.cgi?article=3690&context=fss_papers"]
+
+Of the works we read in Jurisprudence, Robert Cover's "◊em{Nomos} and
+Narrative"◊note-cite["Cover"] brought the most significant additions
+to my perspectives on law. Cover replaces the notion of a single
+"legal system" with a plurality of "normative universes" requiring "no
+state," with legal meaning created in each through "commitment." I
+couldn't tell how literally Cover was equating this commitment with
+potential violence, but he wrote:
+
+◊blockquote{
+The state's claims over legal meaning are, at bottom, so closely tied
+to the state's imperfect monopoly over the domain of violence that the
+claim of a community to an autonomous meaning must be linked to the
+community's willingness to live out its meaning in defiance.
+}
+
+Under this view, state law only has a privileged position because of
+the state's "imperfect monopoly over violence."
+
+I find this theme in the following stanzas.
+
+◊codeblock{
+Others say, Law is our Fate;
+Others say, Law is our State;
+Others say, others say
+Law is no more,
+Law has gone away.
+
+And always the loud angry crowd,
+Very angry and very loud,
+Law is We,
+And always the soft idiot softly Me.
+}
+
+The "soft idiot" could be one who declares something to be law without
+the real commitment either personally or from a community to "live out
+its meaning."
+
+There's a lot more in this poem, and other meaninings you might have
+taken from it. Please share your thoughts!
