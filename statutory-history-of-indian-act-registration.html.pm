a6fda41c98a22ff8d767849d925c7be20749a980
Sancho McCann <sanchom@gmail.com>
2021-05-03 18:11:39 -0700

    Updated front page; added Indian Act history

---
+++ statutory-history-of-indian-act-registration.html.pm
@@ -0,0 +1,188 @@
+#lang pollen
+
+◊define-meta[page-title]{◊@{Statutory History of Eligibility for Registration (Status) under the ◊em{Indian Act}}}
+◊define-meta[short-title]{◊@{◊em{Indian Act} Status (History)}}
+◊define-meta[original-date]{2021-05-02}
+
+◊declare-work[#:id "Hele" #:type "legal-case" #:title "Hele v Attorney
+General of Canada" #:citation "2020 QCCS 2406" #:url
+"https://canlii.ca/t/j8zvm"]
+
+◊declare-work[#:id "Indian Act" #:type "statute" #:title "Indian Act"
+#:volume "RSC" #:year "1985" #:chapter "I-5" #:url
+"https://laws-lois.justice.gc.ca/eng/acts/i-5/FullText.html"]
+
+◊declare-work[#:id "McIvor" #:type "legal-case" #:title "McIvor v
+Canada (Registrar of Indian and Northern Affairs)" #:citation "2009
+BCCA 153" #:url "https://canlii.ca/t/230zn"]
+
+◊declare-work[#:id "Descheneaux" #:type "legal-case" #:title
+"Descheneaux c Canada (Procureur Général)" #:citation "2015 QCCS 3555"
+#:url "https://canlii.ca/t/glzhm"]
+
+On this page, I link to versions of and amendments to the ◊em{Indian
+Act}’s registration rules◊note-cite["Indian Act" #:pinpoint "ss 6, 7"]
+(colloquially referred to as ◊em{status}) from 1951 onward.
+
+First, here is some background material that presents in broad strokes
+how eligibility for registration has changed over the years.
+
+◊itemize{
+
+Assembly of First Nations, "[History of Registration After the Indian Act](https://www.afn.ca/wp-content/uploads/2020/01/09-19-02-06-AFN-Fact-Sheet-History-of-Registration-under-the-Indian-Act-revised.pdf)"
+
+Crown-Indigenous Reglations and Northern Affairs Canada (CIRNAC), "[Background on Indian registration](https://www.rcaanc-cirnac.gc.ca/eng/1540405608208/1568898474141)"
+
+}
+
+These are good explanations. However, to really understand today's
+status eligibility rules, you need to understand the statutory history
+of the relevant sections of the ◊em{Indian Act}. Just look at some of
+the language in today's Act: "Subect to section 7, a person is
+entitled to be registered if ◊elide the name of that person was
+omitted or deleted from the Indian Register ◊elide under subparagraph
+12(1)(a)(iii) pursuant to an order made under subsection 109(1), as
+each provision read immediately prior to April 17, 1985, or any former
+provision of this Act relating to the same subject-matter as any of
+those provisions."◊note-cite["Indian Act" #:pinpoint "s 6(1)(d)"]
+
+I'll first explain a recent decision from the Superior Court of
+Quebec, ◊em{Hele v. Attorney General of Canada}.◊note-cite["Hele"] The
+takeaway is that in order to understand the paths to eligibility
+◊em{today}, you need to understand the paths to ◊em{ineligibility}
+from the past. The evolution of these rules can be difficult to
+follow, so I have collected below the various versions that have
+existed since 1951. The most important historical version for
+application of today's eligibility requirements is the version that
+existed immediately prior to April 17, 1985, but occasionally,
+reference to even older versions will be necessary.
+
+◊heading{◊em{Hele v. Attorney General of Canada}}
+
+This case was about a father (Karl) trying to have his daughter
+(Annora) registered under the ◊em{Indian Act}. Because there was no
+evidence that Annora's mother was entitled to register, Annora's
+eligibility turned on whether Karl was entitled to register under
+s. 6(1) or under s. 6(2).
+
+If Karl was entitled under s. 6(1), then Annora would be entitled to
+register under s. 6(2). If Karl was entitled under s. 6(2), then
+Annora would ◊em{not} be entitled to registration. This is because of
+the "second-generation cutoff" built into the operation of ss. 6(1)
+and 6(2).
+
+◊fig[#:src "assets/hele.png"]
+
+So, what determined whether Karl was entitled under s. 6(1) or under
+s. 6(2)? That depended on his mother Margaret's path to
+re-entitlement.
+
+Karl's mother, Margaret, had purportedly "voluntarily enfranchised" in
+1965 via s. 109(1). She later married a "non-Indian Canadian" in
+1969. If Margaret's voluntary enfranchisement in 1965 was effective,
+then she would have become re-entitled to registration through
+s. 6(1)(d) [re-entitlement after voluntary enfranchisement]. If,
+however, Margaret was never able to voluntarily enfranchise (this was
+the argument Karl was making), she would have instead lost her
+eligibility by "marrying out" in 1969 and then would have become
+re-entitled through s. 6(1)(c)◊note{Today, this is s. 6(1)(a.1).}
+[re-entitlement after marrying out]. This difference is significant
+because if Margaret became re-entitled through s. 6(1)(c),◊note{Today,
+s. 6(1)(a.1).} then Karl ◊em{would} be entitled under
+s. 6(1).◊note{Specifically, s. 6(1)(c.1), at the time of Karl's
+application, or today, s. 6(1)(a.3)(i).} If instead Margaret had
+become re-entitled through s. 6(1)(d) [re-entitlement after voluntary
+enfranchisement], Karl would have only been entitled himself through
+s. 6(2).
+
+Judge Barin, after a quite intensive exercise of statutory
+interpretation, concluded that voluntary enfranchisement was not
+available to Margaret in 1965. The Governor in Council did not have
+"the power to enfranchise unmarried Indian women." Thus, instead of
+voluntarily enfranchising in 1965, Margaret lost her status in 1969
+when she married out. She thus regained eligibility through
+s. 6(1)(c),◊note{Today, s. 6(1)(a.1).}. So, Karl is eligible under
+s. 6(1) and Annora under s. 6(2).
+
+You can see that in order to understand this decision, I needed to
+refer to the ◊em{Indian Act} as it existed prior to 2017 (when Karl
+had applied) and prior to 1985. The point of this blog post is to make
+that research easier for you by linking directly to the historic acts
+and amendments since 1951 that affect the registration eligibility
+rules.
+
+◊heading{Statutory History}
+
+There are many more amendments to the ◊em{Indian Act} than I list
+below. These are only those since 1951 that affected the registration
+eligibility (status) rules.
+
+◊itemize{
+
+◊a[#:href "assets/Indian-Act/SC_1951_(15_George_VI)_c_29.pdf"]{SC 1951
+(15 George VI), c 29}.
+
+◊a[#:href "assets/Indian-Act/RSC_1952_c_149.pdf"]{RSC 1952, c 149}.
+
+◊a[#:href "assets/Indian-Act/SC_1956_c_40.pdf"]{SC 1956, c 40}.
+
+◊a[#:href "assets/Indian-Act/SC_1958_c_19.pdf"]{SC 1958, c 19}.
+
+◊a[#:href "assets/Indian-Act/RSC_1970_c_I-6.pdf"]{RSC 1970, c
+I-6}. This is the version to consult when the current Act refers to
+s. 12 or s. 109 as they read immediately prior to April 17, 1985.
+
+◊a[#:href "assets/Indian-Act/RSC_1985_c_I-5.pdf"]{RSC 1985, c
+I-5}. This was basically a restatement of the Act as it was in force
+previously as part of the [consolidation
+process](https://en.wikipedia.org/wiki/Statutes_of_Canada).
+
+◊a[#:href "assets/Indian-Act/RSC_1985_c_32_(1st_Supp).pdf"]{RSC 1985,
+c 32 (1st Supp), s 4}. This contains the substantive amendments to the
+eligibility/ineligibility provisions and moved them from ss. 11 and 12
+to ss. 6 and 7. It also repealed s. 109 (enfranchisement).
+
+◊a[#:href "assets/Indian-Act/RSC_1985_c_43_(4th_Supp).pdf"]{RSC 1985,
+c 43 (4th Supp), s 1}. Slight amendment relating to deceased
+persons.
+
+}
+
+
+From 2002 onward, the Department of Justice allows you to browse the
+Act as it was in force at any point in time:
+[laws-lois.justice.gc.ca/eng/acts/i-5/PITIndex.html](https://laws-lois.justice.gc.ca/eng/acts/i-5/PITIndex.html). But,
+I'll still link to the two amendments to the registration provisions
+next.
+
+◊itemize{
+
+SC 2010, c 18
+([html](https://laws-lois.justice.gc.ca/eng/annualstatutes/2010_18/FullText.html);
+[pdf](https://laws-lois.justice.gc.ca/PDF/2010_18.pdf)). This was Bill
+C-3, in response to ◊em{McIvor v. Canada}.◊note-see["McIvor"]
+
+SC 2017, c 25
+([html](https://laws-lois.justice.gc.ca/eng/annualstatutes/2017_25/FullText.html);
+[pdf](https://laws-lois.justice.gc.ca/PDF/2017_25.pdf)). This was Bill
+S-3, in response to ◊em{Descheneaux
+c. Canada}.◊note-see["Descheneaux"]
+
+}
+
+◊heading{Doing Your Own Research}
+
+If you want to do your own research into statutory history, you can
+find [Revised Statutes of
+Canada](https://en.wikipedia.org/wiki/Statutes_of_Canada) through
+HeinOnline's database called "Revised Statutes of Canada." Your
+library might subscribe to that. If you're at UBC, here's the [direct
+link](https://heinonline-org.ezproxy.library.ubc.ca/HOL/Index?index=castatutes/rdtutda&collection=castatutes).
+
+And you can find the yearly statutes in a database called "Annual
+Statutes." Here's the [direct
+link](https://heinonline-org.ezproxy.library.ubc.ca/HOL/Index?collection=caotp)
+for those at UBC.
+
+If you are aware of more readily available sources, please link them
+in the comments!
