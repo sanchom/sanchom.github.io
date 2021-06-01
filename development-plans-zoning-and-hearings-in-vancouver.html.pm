8486045986b8b249f1262b3b0cb95a54f85bb23a
Sancho McCann <sanchom@gmail.com>
2021-06-01 09:39:24 -0700

    Added post about vancouver zoning

---
+++ development-plans-zoning-and-hearings-in-vancouver.html.pm
@@ -0,0 +1,270 @@
+#lang pollen
+
+◊define-meta[page-title]{Development plans, zoning, and public hearings in Vancouver}
+◊define-meta[short-title]{Public hearings in Vancouver}
+◊define-meta[original-date]{2021-05-31}
+◊define-meta[snippet]{Why Vancouver Council can't waive rezoning hearings like other municipalities can}
+
+◊declare-work[#:id "Scenescapes" #:type "book" #:author-given "Daniel
+Aaron" #:author-family "Silver" #:author2-given "Terry Nichols"
+#:author2-family "Clark" #:title "Scenescapes: How Qualities of Place
+Shape Social Life" #:year "2016" #:publisher "University of Chicago
+Press" #:publisher-location "Chicago"]
+
+◊declare-work[#:id "LGA" #:type "statute" #:title "Local Government
+Act" #:volume "RSBC" #:year "2015" #:chapter "1" #:short-form "*LGA*"
+#:url
+"https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/r15001_00_multi"]
+
+◊declare-work[#:id "VC" #:type "statute" #:title "Vancouver Charter"
+#:volume "SBC" #:year "1953" #:chapter "55" #:url
+"https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/vanch_00_multi"]
+
+◊declare-work[#:id "RS-1" #:type "custom" #:custom-format "City of
+Vancouver, *Consolidated Zoning and Development By-Law*, RS-1 District
+Schedule" #:url
+"https://bylaws.vancouver.ca/zoning/zoning-by-law-district-schedule-rs-1.pdf"
+#:short-form "RS-1 District Schedule" #:display-url? "yes"]
+
+◊declare-work[#:id "Pitt Polder" #:type "legal-case" #:title "Pitt
+Polder Preservation Society v Pitt Meadows (District)" #:citation
+"2000 BCCA 415" #:url "https://canlii.ca/t/5315"]
+
+◊declare-work[#:id "Johnston" #:type "thesis" #:title "Local
+Government Rural Land Use Planning in BC" #:author "Terry Johnston"
+#:institution "University of British Columbia" #:thesis-description
+"MA thesis" #:year "1990" #:url
+"https://open.library.ubc.ca/cIRcle/collections/ubctheses/831/items/1.0302384"]
+
+◊declare-work[#:id "CPR" #:type "legal-case" #:title "Canadian Pacific
+Railway Co v Vancouver (City)" #:citation "2006 SCC 5" #:url
+"https://canlii.ca/t/1mm2r"]
+
+◊declare-work[#:id "2nd reading" #:type "debate" #:title "Bill C-62,
+Municipal Amendment Act, 1985" #:reading "2nd reading" #:jurisdiction
+"British Columbia" #:proceedings "Debates of the Legislative Assembly"
+#:legislative-body "33-3" #:date "21 November 1985" #:url
+"https://www.leg.bc.ca/documents-data/debate-transcripts/33rd-parliament/3rd-session/33p_03s_851121a"
+#:short-form "*MAA* 2nd Reading"]
+
+◊declare-work[#:id "CPR BCCA" #:type "legal-case" #:title "Canadian
+Pacific Railway Co v Vancouver (City)" #:citation "2004 BCCA 192"
+#:url "https://canlii.ca/t/1gvkh" #:short-form "*CPR* BCCA"]
+
+◊declare-work[#:id "Municipal Act 1979" #:type "statute" #:title
+"Municipal Act" #:volume "RSBC" #:year "1979" #:chapter "290"]
+
+◊declare-work[#:id "MAA" #:type "statute" #:title "Municipal Amendment
+Act, 1985" #:volume "SBC" #:year "1985" #:chapter "79"]
+
+Vancouver has several ◊em{[official development
+plans](https://vancouver.ca/home-property-development/zoning-and-land-use-policies-document-library.aspx#regulation-development-plans)}
+(ODPs). These plans limit the uses that are allowed in certain
+areas. For example, the [Arbutus Corridor
+ODP](https://bylaws.vancouver.ca/odp/odp-arbutus-corridor.pdf)
+designates the corridor for use only as a public thoroughfair. The
+[Southeast Granville Slopes
+ODP](https://bylaws.vancouver.ca/odp/odp-southeast-granville-slopes.pdf)
+sets a maximum height for buildings and lists the uses that are
+allowed in the area ("a mixture of residential uses, restaurant and
+entertainment activities, other commercial activities, and intense
+public activities").
+
+If this were any other municipality in British Columbia, these plans
+would streamline rezoning: they would allow Council to waive the
+requirement for a public hearing when the rezoning is consistent with
+an ODP for the area. But Vancouver does not get this benefit from its
+ODPs. In this blog post, I present how this difference came to be and
+argue that the province should consider bringing the ◊em{Vancouver
+Charter} in line with the ◊em{Local Government Act} on this point.
+
+◊heading{Zoning and public hearings}
+
+Municipalities use zoning bylaws to control land
+use.◊note-see-generally["Scenescapes" #:pinpoint "Ch 7"] The province
+has given municipalities the power to divide the land within their
+boundaries into zones of any number, shape, or size and within each,
+to regulate the use of the land.◊note{◊see["LGA" #:pinpoint "s 749"
+#:terminal ";"] ◊cite["VC" #:pinpoint "s 565"] The ◊em{Local
+Government Act} applies to municipalities other than Vancouver. The
+◊em{Vancouver Charter} applies only to Vancouver.} Common restrictions
+are the heights of buildings, maximum floor-space ratio, and the kinds
+of uses that are allowed within a zone.◊note-see-eg["RS-1"]
+
+Zoning significantly affects the kind of community that we
+create.◊note-see-generally["Scenescapes" #:pinpoint "Ch 7"] So when a
+municipal council makes, amends, or repeals a zoning bylaw, it must
+first hold a public hearing (with one important exception, presented
+later).◊note{◊see["LGA" #:pinpoint "s 566" #:terminal ";"] ◊cite["VC"
+#:pinpoint "s 464"]} These public hearings are not as simple as
+council just listening to speakers at a regular meeting. These public
+hearings have strict notice requirements, people who would be affected
+by the bylaw have procedural fairness guarantees and a right to be
+heard, and council has disclosure obligations. The Court of Appeal for
+British Columbia has said:
+
+◊blockquote{
+
+[44] ◊elide There is an abundance of jurisprudence on the fundamental
+importance of notice to the legitimacy of the decision-making process
+in land use and zoning cases.  Simply put, failure to comply with the
+notice requirements in relation to a public hearing undermines the
+opportunity afforded the public to participate in the decision-making
+process. Failure to comply with the notice requirement for a public
+hearing on proposed land use or zoning bylaws generally results in the
+bylaw being quashed.
+
+[45] A public hearing on land use and zoning bylaws serves at least
+two important functions: it provides an opportunity for those whose
+interests might be affected by such a decision to make their views
+known to the decision-maker and it gives the decision-maker the
+benefit of public examination and discussion of the issues surrounding
+the adoption or rejection of the proposed bylaw.
+
+[46] Procedures aimed at ensuring a minimum standard of rationality in
+the decision-making process are more likely to enhance the quality of
+the decision and the public’s acceptance of it than decisions based on
+undisclosed information, or on incomplete or ill-considered facts.
+
+[47] As well, participatory procedures such as public hearings on land
+use or zoning bylaws tend to dispel perceptions of arbitrariness, bias
+or other impropriety on the part of local government in the
+decision-making process and tend to enhance public acceptance of such
+decisions. Put another way, the perception, if not the fact, of
+arbitrariness or bias is more likely to arise if the duty to ensure
+procedural fairness is not observed.◊note-cite["Pitt Polder"]
+
+}
+
+Vancouver's ◊em{[Procedure
+Bylaw](https://bylaws.vancouver.ca/12577c.PDF)} describes the process
+that Vancouver Council has adopted for public hearings. It is rightly
+onerous and time-consuming.
+
+◊fig[#:src "assets/public-hearing-notice.png" #:width "700px"]{This is
+a sample notice for a public hearing from 2019.}
+
+◊heading{Official community/development plans}
+
+Official community plans and official development plans work at a
+level above zoning.
+
+All local governments other than Vancouver have a tool available to
+them called an ◊em{official community plan} (OCP). The near-equivalent
+in Vancouver is called an ◊em{official development plan} (ODP). These
+describe a vision for planning more generally than at the level of
+zoning. They have certain mandatory elements, including "location,
+amount, type and density of residential development required to meet
+anticipated housing needs over a period of at least 5 years," and "the
+approximate location, amount and type of present and proposed
+commercial, industrial, institutional, agricultural, recreational and
+public utility land uses."◊note{See the full list at ◊cite["LGA"
+#:pinpoint "s 473"]}
+
+Official community plans cannot be adopted without a public
+hearing.◊note-see["LGA" #:pinpoint "s 464"] Once a council adopts an
+OCP, council becomes constrained by the OCP. "All bylaws enacted or
+works undertaken by a council... after the adoption of... an official
+community plan... must be consistent with the relevant
+plan."◊note-cite["LGA" #:pinpoint "s 478(2)"] However, a council also
+gains some efficiency if it develops consistenly with the OCP: council
+may ◊em{waive} the public hearing for zoning bylaws that are
+consistent with the OCP.◊note-see["LGA" #:pinpoint "464(2)"]
+
+Vancouver's official ◊em{development} plans similarly constrain
+Council. However, there are two major differences. First, the
+◊em{Vancouver Charter} does not require a public hearing before
+passing an ODP. Only when the ODP designates a heritage conservation
+area does Council need to hold a public hearing.◊note{◊see["VC"
+#:pinpoint "s 562(3)" #:parenthetical "“Before adopting [an ODP
+bylaw], if the official development plan designates a heritage
+conservation area... the Council must hold a public hearing”"]
+◊see-also["CPR" #:pinpoint "para 40" #:parenthetical "“[t]he Vancouver
+Charter imposes no statutory requirement to hold a public hearing
+before adopting an ODP”"]} Second, even when a proposed zoning bylaw
+is consistent with an existing ODP, Council cannot waive the public
+hearing requirement for the zoning bylaw.
+
+Again, the ◊em{Local Government Act} (applying to all municipalities
+other than Vancouver) says that "[a] local government may waive the
+holding of a public hearing on a proposed zoning bylaw if (a) an
+official community plan is in effect for the area that is subject to
+the bylaw, and (b) the bylaw is consistent with the official community
+plan."◊note-cite["LGA" #:pinpoint "s 464(2)"] There just is no
+equivalent in the ◊em{Vancouver Charter}.◊note{The ◊em{Vancouver
+Charter} does incorporate several portions of the ◊em{Local Government
+Act}, but not Section 464. ◊see["VC" #:pinpoint "s 2.1"]}
+
+◊heading{Why is Vancouver different?}
+
+They didn't start out different.
+
+As recently as 1979, all municipalities in British Columbia were in
+roughly the same position that Vancouver is in today on this
+issue. Section 721(1) of the ◊em{Municipal Act}◊note{The precursor to
+today's ◊em{Local Government Act}.} as it existed in 1979 simply said,
+"A zoning bylaw shall not be adopted, amended or repealed except after
+a hearing under section 720."◊note-cite["Municipal Act 1979"
+#:pinpoint "s 721"] There was no possibility to waive that public
+hearing, even when the community had an official community plan in
+place. And those OCPs also required a public
+hearing.◊note-see["Municipal Act 1979" #:pinpoint "s 711"
+#:parenthetical "“[t]he council shall not designate a community plan
+as an official community plan unless it has held a public
+hearing...”"]
+
+By the early 1980s, municipalities were getting frustrated with
+inefficiencies in the planning process, particularily the
+double-hearing requirement.◊note-see["Johnston" #:pinpoint "82"
+#:parenthetical "“it is clear that the dual amendments
+required... were a duplication of effort”"] Bill Ritchie, Municipal
+Affairs Minister at the time, even said, "I fully believe once an
+official municipal plan is in place, there is no further need for
+planning personnel."◊note-cite["Johnston" #:pinpoint "84"]
+
+The result was Bill 62, the ◊em{Municipal Amendment Act,
+1985}.◊note-cite["MAA"] It introduced the public-hearing waiver for
+zoning bylaws that are consistent with the community plan: "[w]here an
+official community plan is in effect for the area that is subject to a
+proposed zoning bylaw and the proposed bylaw is consistent with the
+plan, a local government may waive the holding of a public hearing on
+the proposed bylaw."◊note{◊cite["MAA" #:pinpoint "s 8" #:terminal ","]
+which added the quoted text as s. 956(4) of the ◊em{Municipal Act}.}
+Municipal Affairs Minister Bill Ritchie presented the following
+motivation: "Bill 62 is designed to streamline the community planning
+process. It is designed to regulate land use controls, especially in
+the rural areas, to facilitate faster and more flexible responses to
+development proposals, and to provide greater certainty for land
+investors and local residents."◊note-cite["2nd reading" #:pinpoint
+"7054" #:speaker "Hon Bill Ritchie"]
+
+The impetus for the 1985 amendment was the double-hearing requirement
+that had existed under the ◊em{Municipal Act} and which applied to
+municipalities other than Vancouver. Their OCPs required public
+hearings and so did their rezonings, even when consistent with their
+OCP. Vancouver has never been legally subject to this double-hearing
+requirement, other than when the OCP designates a heritage
+conservation area. So the amendment focused on the the ◊em{Municipal
+Act}, and the ◊em{Vancouver Charter} got left behind. Nonetheless,
+even though not required, Vancouver has a policy to hold public
+hearings before enacting official development plans.◊note-see["CPR
+BCCA" #:pinpoint "para 94" #:parenthetical "“it has been the policy of
+the City to hold a public hearing before enacting [an ODP bylaw]”"]
+The status quo today is that Vancouver Council subjects itself to a
+double-hearing process: one for the ODP and another for rezonings even
+when consistent with the ODP.
+
+In the early 1980s, the complaint was that two stages of public
+hearing was a "duplication of effort" and that planning "had a
+tendency to create unnecessary bureaucracy."◊note-cite["Johnston"
+#:pinpoint "82"] Vancouver is facing similar issues today. Vancouver
+Council and the province should deliberately consider whether this
+distinction between the ◊em{Local Government Act} and the
+◊em{Vancouver Charter} continues to make sense. If Vancouver Council
+is going to use a public hearing process for its ODPs, it should get
+the same efficiency reward that other municipalities get (the ability
+to waive the public hearing for rezonings consistent with the
+ODP).◊note{And if Vancouver Council wants to retain flexibility, the
+public-hearing waiver (for rezonings that are consistent with the ODP)
+could be conditioned on the ODP having been subject to a public
+hearing.}
