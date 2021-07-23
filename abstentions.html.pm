fa54d3814e3a947843ec0a471501e94adeb020c7
Sancho McCann <sanchom@gmail.com>
2021-07-23 11:29:59 -0700

    Added abstention follow-up

---
+++ abstentions.html.pm
@@ -0,0 +1,203 @@
+#lang pollen
+
+◊define-meta[page-title]{Abstentions at Vancouver City Council: Follow-up}
+◊define-meta[short-title]{Abstentions}
+◊define-meta[original-date]{2021-07-23}
+◊define-meta[snippet]{Some additional details about motions in which an abstention tipped the vote}
+
+In this blog post, I share some additional details about the
+abstentions I mention in [my op-ed that appeared in the Vancouver Sun
+on July 21,
+2021](https://vancouversun.com/opinion/sancho-mccann-when-a-vancouver-city-councillor-abstains-it-is-a-vote-in-favour). In
+that piece, I argued that "[s]ince an abstention is a vote in favour,
+when a councillor cannot in good conscience vote in favour of a
+motion, they should vote in opposition."
+
+I also said that "I identified 14 motions heard by this council that
+would not have passed had the abstentions not counted in favour."
+
+This post provides a slight correction (◊b{there were only thirteen,
+not fourteen}) and shows you the specific motions and their
+effects. They span the gamut from very minor wording changes, to
+substantive amendments to motions, giving directions to staff on a
+variety of matters, and waiving presentations from staff.
+
+Many of the motions in which an abstention made a difference to the
+vote were motions to amend a higher-level motion. The wording of the
+amendment often (but not always) ended up in the ultimate motion that
+was carried.
+
+To be clear, the argument in my op-ed would be unchanged even if I
+discovered no motions in which an abstention tipped the scales. But it
+is interesting to see the motions where abstentions made a difference
+to the vote. I also hope this might familiarize people with the Open
+Data Portal and how to understand Council minutes.
+
+◊heading{Correction}
+
+First though: the correction. The [Open Data Portal reported that vote
+4072](https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=4072&sort=vote_detail_id)
+was a motion that carried, despite having more votes in opposition
+than explicitly in favour. My previously analysis counted this as a
+motion in which the abstention made a difference to the vote.
+
+◊fig[#:src "assets/odp-vote-4072.png"]{The data from the Open Data Portal for vote 4072.}
+
+However, the [minutes for that
+vote](https://council.vancouver.ca/20190227/documents/pspc20190227min.pdf)
+(see p. 12) clearly show this to be a motion that lost. The abstention
+did not make a difference.
+
+◊fig[#:src "assets/minutes-vote-4072.png"]{The minutes for vote 4072.}
+
+I have sent an email to the Open Data Portal maintainers to suggest
+correcting the data.
+
+◊heading{The thirteen motions}
+
+Here, I link to and describe the thirteen motions in which an
+abstention made a difference to the outcome of a vote. Again, this is
+just for interest and completeness.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=4096&sort=vote_detail_id"]{4096}
+(◊a[#:href
+"https://council.vancouver.ca/20190227/documents/pspc20190227min.pdf"]{Minutes})}
+
+This related to an emergency interim zoning policy for Broadway
+Corridor to UBC. Vote 4096 was on a motion to amend. It changed the
+text of the ultimate motion. Instead of directing "staff to extend the
+Broadway Plan interim rezoning policy," the motion changed the wording
+to direct staff to "explore the implications of extending" it. This
+wording made it into the ultimate motion that carried.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=4112&sort=vote_detail_id"]{4112}
+(◊a[#:href
+"https://council.vancouver.ca/20190227/documents/pspc20190227min.pdf"]{Minutes})}
+
+This related to the same emergency interim zoning policy just
+discussed. Vote 4112 was on another motion to amend. It added some
+text to the ultimate motion that directed staff to "consider
+Affordable Home Ownership specifically in partnerships with BC Housing
+Affordable Home Ownership program...". This wording made it into the
+ultimate motion that carried.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=4430&sort=vote_detail_id"]{4430}
+(◊a[#:href
+"https://council.vancouver.ca/20190515/documents/pspc20190515min.pdf"]{Minutes})}
+
+This related to the False Creek to Fraser River Blueways. Vote 4430
+was on a motion to amend. It changed the wording that asked the Park
+Board to vote. The wording made it into the ultimate motion that
+carried.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=4433&sort=vote_detail_id"]{4433}
+(◊a[#:href
+"https://council.vancouver.ca/20190515/documents/pspc20190515min.pdf"]{Minutes})}
+
+This related to the same False Creek to Fraser River Blueways motion
+just discussed. Vote 4433 was on a motion to amend. It changed the
+wording that asked the Park Board to vote. The wording made it into
+the ultimate motion that carried.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=5035&sort=vote_detail_id"]{5035}
+(◊a[#:href
+"https://council.vancouver.ca/20191023/documents/pspc20191023min.pdf"]{Minutes})}
+
+This was a motion at the Standing Committee of Council on Policy and
+Strategic Priorities. They were recommending changes to the Procedure
+By-law. Vote 5035 was a vote on a motion to set the time limit for
+"representative speakers" to five minutes. This limit made into the
+wording that would ultimately carry and be recommended to council.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=5642&sort=vote_detail_id"]{5642}
+(◊a[#:href
+"https://council.vancouver.ca/20200310/documents/regu20200310min.pdf"]{Minutes})}
+
+This related to a motion on supporting gender equity and diversity in
+Vancouver City Council. Vote 5642 was a motion to amend. It changed
+the wording to "support the volunteer reporting of" diversity. But
+this was undone in vote 5645 because it didn't fit gramatically or
+logically with the rest of the motion's text.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=5790&sort=vote_detail_id"]{5790}
+(◊a[#:href
+"https://council.vancouver.ca/20200428/documents/regu20200428min.pdf"]{Minutes})}
+
+This related to a motion about COVID-19 pandemic impacts. Vote 5790
+was a motion to amend. It added direction to staff to "consider
+specific pay parking exemptions for essential and health care
+workers...". This wording made it into the ultimate motion that
+carried.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=5904&sort=vote_detail_id"]{5904}
+(◊a[#:href
+"https://council.vancouver.ca/20200526/documents/regu20200526min.pdf"]{Minutes})}
+
+This related to a motion following a presentation from staff on
+COVID-19 pandemic impacts. Vote 5904 was a motion to amend. It added
+direction to staff to "develop a decision making framework for
+prioritizing operating budget reductions...". This wording made it
+into the ultimate motion that carried.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=6068&sort=vote_detail_id"]{6068}
+(◊a[#:href
+"https://council.vancouver.ca/20200610/documents/cfsc20200610min.pdf"]{Minutes})}
+
+Vote 6068 was on a referral motion. It referred the motion entitled
+"Short Term Landlords --- Long Term Protections for Renters and
+Hotels" (which was about AirBNB-type accomodations) to staff.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=6520&sort=vote_detail_id"]{6520}
+(◊a[#:href
+"https://council.vancouver.ca/20201020/documents/regu20201020min.pdf"]{Minutes})}
+
+This related to scheduling. Vote 6520 was a motion to amend. It added
+wording to direct staff to not schedule public hearings in 2021
+earlier than 6pm, Mondays through Thursday and that public hearings
+should be evenly distributed month to month. This wording made it into
+the ultimate motion that carried.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=6852&sort=vote_detail_id"]{6852}
+(◊a[#:href
+"https://council.vancouver.ca/20210119/documents/phea20210119min.pdf"]{Minutes})}
+
+This related to a staff presentation about a rezoning. Vote 6852 was
+on a motion to waive the staff presentation. It carried, but was
+immediately reconsidered in vote 6854 and lost that time, the
+abstainers from vote 6852 having switched their votes to votes in
+opposition. The staff presentation was heard.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=6883&sort=vote_detail_id"]{6883}
+(◊a[#:href
+"https://council.vancouver.ca/20210119/documents/regu20210119min.pdf"]{Minutes})}
+
+This related to a rezoning application. Vote 6883 was on a motion to
+amend an amendment. It changed the amendment to try to add wording to
+request a legal opinion about the language used in some zoning
+guidelines. Vote 6883 carried and thus successfully amended the
+amendment. But the amendment itself lost and so this wording did not
+end up into the ultimate motion that carried.
+
+◊sub-heading{Vote ◊a[#:href
+"https://opendata.vancouver.ca/explore/dataset/council-voting-records/table/?disjunctive.council_member&refine.vote_number=7435&sort=vote_detail_id"]{7435}
+(◊a[#:href
+"https://council.vancouver.ca/20210623/documents/pspc20210623min.pdf"]{Minutes})}
+
+This related to electric kick-scooters. Vote 7435 was on a motion to
+amend. It added some wording to exclude the application of a part of a
+by-law. This amendment carried, but was immediately reconsidered and
+then ruled out of order because it was too similar to something
+proposed within the previous 365 days. So, the wording associated with
+vote 7435 did not end up in the ultimate motion that carried.
