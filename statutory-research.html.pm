2b4fab0b39b860c42f1eff297214eaa62d872f81
Sancho McCann <sanchom@gmail.com>
2021-07-24 17:59:20 -0700

    Added beginnings of statutory research series

---
+++ statutory-research.html.pm
@@ -0,0 +1,260 @@
+#lang pollen
+
+◊define-meta[page-title]{Statutory research series}
+◊define-meta[short-title]{Statutory research}
+◊define-meta[original-date]{2021-07-24}
+◊define-meta[featured-image-url]{assets/1921-Bill-12.png}
+◊define-meta[snippet]{How to research statutory and legislative history of Canadian statutory law.}
+
+Welcome to this short series on statutory research. I want to help you
+research statutory and legislative history of Canadian statutory
+law.◊note{By ◊em{statutory history}, I mean the evolution of an Act
+over time. By ◊em{legislative history}, I mean the record of the
+legislative process that leads to an enacted statute. And for the
+meaning of ◊em{Act} and ◊em{statute}, read on!} This will allow you to
+understand the content of an Act at any point in time and to find out
+what legislators said about bills prior to their passage into law.
+
+By the end of this series, you should be able to do the following.
+
+◊ol{
+◊li{Identify the statutory sources of an Act and any amendments that have
+occured to an Act throughout its history. That means finding the
+statutes and bills from which the Act and any amendments to it arose.}
+
+◊li{Find the legislative debates associated with those bills.}
+
+◊li{Determine when the Act (or an amendment to it) came into force.}
+}
+
+This page will be a work-in-progress that I think will eventually
+expand to four small modules. I will update this page as I complete
+the material for each.
+
+◊b{What this series omits, but I want you to be aware of, are methods
+of research into Indigenous laws and legal traditions.} In the series
+developed on this page, you will see that even researching what Canada
+has written down about its legislated law can be complicated. It is
+all that more complex to research Indigenous law that may or may not
+be in written form and that is in various stages of revitalization,
+depending on the particular Indigenous nation. To learn more about
+this, I recommend reading about the work of the [Indigenous Law
+Research Unit](https://ilru.ca/) at the University of Victoria; a
+[special issue of the McGill Law
+Journal](https://lawjournal.mcgill.ca/issue/volume-61-issue-4-2016/)
+on Indigenous law, legal orders, and traditions; and a collection of
+essays edited by Michael Asch, John Borrows, and James Tully,
+◊a[#:href
+"https://www.goodreads.com/book/show/39653926-resurgence-and-reconciliation"]{◊em{Resurgence
+and Reconciliation: Indigenous--Settler Relations and Earth
+Teachings}}.
+
+I also want to emphasize, especially for anyone coming to this series
+who is not in law school, that ◊b{this is not how to figure out what
+◊red{The Law} is}. It is only how to research one kind of material
+that contributes to the development of Canadian law. Law is made in
+many ways, in many sites, with various materials and through
+relationships and human commitments to meanings. What the legislature
+has chosen to declare through statute is only one part of how we
+create legal meaning together.
+
+Finally, before embarking on any of the research I describe in this
+series, always check whether somebody else has done some of the work
+for you. There may be a journal article, a court decision, or even a
+Wikipedia article that outlines the rough statutory history of an
+Act. But even if you use someone else’s work as a starting point, you
+will often want to check their work for yourself: did they get every
+date exactly right? did they miss any amendments? So whether you have
+to start from scratch or if you are starting from someone else's work,
+you’ll need to understand what I present in this series.
+
+◊heading{Part 1: The materials}
+
+This first Part covers the ◊em{materials} you will encounter during
+this type of research. These materials are: bills, statutes, Acts
+including amending Acts, Revised Statutes, official consolidations,
+unofficial consolidations, and Hansard records. I will present these
+all in this first Part. Another important source is the Canada Gazette
+(and the provincial equivalents), but I will leave discussion of the
+Gazettes until the final Part in this series.
+
+Here are some rough descriptions of these materials I just mentioned.
+
+◊b{A bill}: the piece of legislation that is developed in Parliament
+(or a provincial legislature).◊note{Going forward, I will not
+distinguish between Parliament and provincial legislatures unless
+significant.} A bill is what the Parliament drafts, debates, amends,
+and passes.◊note{A bill is often originally drafted by the executive
+branch of government for introduction to Parliament.} A bill passed by
+Parliament becomes an Act upon Royal Assent.◊note{This series will not
+focus on the full internal process of what happens in Parliament, but
+[here is a good discussion between Professor Craig Forcese and
+Professor John Mark
+Keyes](https://www.youtube.com/watch?v=jRWyJddRIPU) on the legislative
+process.}
+
+In Parliament, bills are labelled starting with a ◊em{C-} if they
+originate in the House of Commons and they are labelled starting with
+an ◊em{S-} if they originate in the Senate. Numbering starts over at
+the beginning of each legislative session. Some Acts that gained notoriety prior
+to their passage are often known by their bill number for quite some
+time because this is how the media and commentators would have been
+referring to them.
+
+For example, in 2021, there was a [Bill
+C-10](https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10926636)
+("An Act to amend the Broadcasting Act and to make consequential
+amendments to other Acts"). Look at some of the headlines: "Heritage
+Minister asks senators to focus on passing Bill C-10," "Feds admit
+Bill C-10 faces opposition across the country." These are ambiguous
+without context, because there is a Bill C-10 in every session of
+Parliament. See e.g. [Bill C-10 from the 43rd Parliament, 1st
+Session](https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=8139986),
+and [Bill C-10 from the 42nd Parliament, 1st
+Session](https://www.parl.ca/LegisInfo/BillDetails.aspx?Language=E&billId=10687311).
+
+◊b{Hansard}. ◊em{Hansard} refers to the record of what the legislature
+did with and said about a bill during its progress through
+Parliament.◊note{Named after the first official publisher of
+Parliamentary debates in the British Parliament.}
+
+◊b{A statute}. A statute is the written text of the Act as ultimately
+enacted by Parliament. These are published in annual volumes entitled
+◊em{Statutes of Canada} (or more simply, ◊em{Annual
+Statutes}).◊note{Provinces have an equivalent.}
+
+◊b{An Act} has a dual-meaning. It can refer to the enacted Bill (the
+thing that receives Royal Assent and which is published as a statute
+in the annual volume). It also refers to the evolving, abstract object
+that gets amended over time. For example, the ◊em{Assisted Human
+Reproduction Act} is the title of a statute that was passed in
+2004. It has been amended since then and the name ◊em{Assisted Human
+Reproduction Act} continues to refer to the current and changing
+content of that Act.
+
+◊b{Revised Statutes}. Several times in Canada's history (1886, 1906,
+1927, 1952, 1970, 1985), Parliament has passed what are called
+◊em{Revised Statutes}. These are when Parliament brings together all
+amendments that have occurred to all the existing Acts over time and
+enacts an updated version of each of those Acts. These updated
+versions incorporate directly into their text the most up-to-date
+amended language. The Revised Statutes have hundreds of chapters, one
+for each Act that was in force at the time.
+
+◊fig[#:src "assets/RSC-1985-TOC.png"]{The first page of the table of
+contents of the 1985 Revised Statutes of Canada. Each Act that was in
+force at the time gets its own chapter.}
+
+In between the publication of Revised Statutes, you have to put
+together the content of an Act by looking at all amendments that have
+happened to an Act since its original passage or since the most recent
+Revised Statute. But ◊em{consolidated} acts can help you with this.
+
+◊b{Unofficial consolidations}, from providers like WestLaw, CanLii, or
+Canada's Justice Laws website (prior to 2009) attempt to present what
+the content of an Act is right now or at any point in time (going back
+only so far, though). See e.g. ◊a[#:href
+"https://www.canlii.org/en/bc/laws/stat/sbc-1953-c-55/latest/sbc-1953-c-55.html#history"]{CanLii's
+unofficial consolidation(s) of the ◊em{Vancouver Charter} from 2008
+until now}. These are generally helpful only for recent
+decades. E.g. You won't find good unofficial consolidations for Acts
+as they existed in 1925.
+
+◊b{Official consolidations} (e.g. [Canada's Justice
+Laws](https://laws-lois.justice.gc.ca/eng/) website since 2009)
+provide authoritative evidence of the content of an Act is right now
+or at at any point in time (again, only going back so far).◊note{This
+[note from the Department of
+Justice](https://laws-lois.justice.gc.ca/eng/ImportantNote/) explains
+what changed in 2009.}
+
+The decision to produce official revisions and/or consolidations and
+the process for doing so differs between federal Parliament and
+provincial legislatures and the practices have changed over time. In
+this series, I will be focusing on the approach take by the federal
+Parliament and the BC legislature. If you're doing work in another
+provice, these general principles should help you understand the
+approach taken by your province, but you'll need to take some time to
+understand the particular system(s) used in your jurisdiction.
+
+◊sub-heading{An example: the ◊em{Copyright Act}}
+
+For the rest of this Part, I will present a federal Act, the
+◊em{Copyright Act}, and point out how all the materials I just
+presented contribute to the product that we see today.
+
+◊b{Today, an official consolidation}
+
+First, to see the ◊em{Copyright Act} as it exists today, we can rely
+on the ◊em{official consolidation} provided by Canada's Justice Laws
+website. Here are direct links to the full, up-to-date content of the
+Act as officially consolidated:
+[HTML](https://laws-lois.justice.gc.ca/eng/acts/c-42/FullText.html);
+[PDF](https://laws-lois.justice.gc.ca/PDF/C-42.pdf). It incorporates
+the effects of all amendments that are in force.
+
+◊b{Original}
+
+While it is debatable what the "original" ◊em{Copyright Act} was, a
+version with the structure that has lasted until today was passed in
+1921.◊note{Its actual short title was ◊em{The Copyright Act,
+1921}. The year was part of the name! This can get confusing,
+especially since when referring to the Act as later amended, it would
+still be called ◊em{The Copyright Act, 1921}. There were also
+precursor ◊em{Copyright Act}s, but this was the first Act that pulled
+all the parts together into a single Act.} It was published in the
+annual volume ◊em{11--12 George V} in 1921,◊note{At this time, the
+annual statutes were titled using the [regnal
+year](https://en.wikipedia.org/wiki/Regnal_year).} and you can read it
+[here](assets/SC-1921-c-24).
+
+◊b{1985 official revised version}
+
+The ◊em{Copyright Act} was around during Canada's most recent Revised
+Statute project (1985), so it was included as a chapter there (chapter
+C-42). You can read that chapter [here](assets/RSC-1985-C-42.pdf).
+
+◊b{A 2012 amendment}
+
+The ◊em{Copyright Act} has been amended many times between 1985 and
+today, including in 2012 when Parliament made many changes to the fair
+dealing user rights. The 2012 amending Act can be found in the [Annual
+Statutes of Canada 2012, Chapter
+20](https://laws-lois.justice.gc.ca/eng/AnnualStatutes/index2012.html). Here
+is its full text
+([HTML](https://laws-lois.justice.gc.ca/eng/AnnualStatutes/2012_20/FullText.html);
+[PDF](https://laws-lois.justice.gc.ca/PDF/2012_20.pdf)). It was
+entitled the ◊em{Copyright Modernization Act} and went through
+Parliament being known as Bill C-11. You can follow the [life of Bill
+C-11 on
+LegisInfo](https://www.parl.ca/legisinfo/BillDetails.aspx?Language=E&billId=5134851),
+including links to committee hearings and Hansard records.
+
+◊b{◊em{Copyright Act} summary}
+
+I showed you a very early version published in 1921. I skipped over
+many amendments but showed you that the 1985 Revised Statutes included
+the ◊em{Copyright Act} at Chapter C-42. And I showed you an amending
+Act: developed as Bill C-11, published as a statute in the Annual
+Statutes of 2012, the effects of which are now part of the current
+official consolidation on the Justice Laws website.
+
+◊sub-heading{Concluding Part 1}
+
+In this Part, I have only introduced you to the materials involved in
+statutory research and by example showed you some of the ways of
+accessing them. You should know what a bill is; what a statute is; the
+dual-meaning of the word ◊em{Act} and that Acts can evolve over time;
+that governments may create official consolidations of Acts and that
+the Parliament very occasionally creates Revised Statutes.
+
+In my examples, I have mostly shown you the simplest, modern modes of
+accessing these materials (e.g. the official consolidation;
+Parliament’s LegisInfo portal which links together bills and Hansard
+in a helpful way). Once you go back a bit further in time, it becomes
+more complicated to identify the evolution of an Act over time and to
+identify the associated statutes, bills, and Hansard records.
+
+That is what I will share with you in the following Parts: how to
+actually ◊em{find} these materials and various methods of research
+that are effective across different eras.
