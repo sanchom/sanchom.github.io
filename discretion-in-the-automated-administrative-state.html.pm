070e2aa8b90c4cb7427147234c4e2d4a98a678a2
Sancho McCann <sanchom@gmail.com>
2022-04-13 21:50:02 -0700

    Publishing pre-print of CJLJ paper

---
+++ discretion-in-the-automated-administrative-state.html.pm
@@ -0,0 +1,45 @@
+#lang pollen
+
+◊define-meta[page-title]{Discretion in the Automated Administrative State}
+◊define-meta[short-title]{Automated discretion}
+◊define-meta[snippet]{An article about the effects of algorithms on the relationships within the administrative state.}
+◊define-meta[original-date]{2022-04-13}
+
+◊declare-work[#:id "McCann" #:type "article" #:author "Sancho McCann"
+#:title "Discretion in the Automated Administrative State" #:journal
+"Can JL & Jur" #:forthcoming "2022" #:url
+"assets/McCann-Discretion-in-the-Automated-Administrative-State-2022.pdf"]
+
+◊cite["McCann"]◊note{That link takes you to the final draft as
+accepted for publication but prior to final edits and
+formatting. Please consult the published version for the version of
+record and accurate pagination: [Canadian Journal of Law and
+Jurisprudence](https://www.cambridge.org/core/journals/canadian-journal-of-law-and-jurisprudence).}
+
+◊a[#:href "assets/McCann-Discretion-in-the-Automated-Administrative-State-2022.pdf"]{
+◊fig[#:src "assets/discretion-snippet.png" #:width "300px"]}
+
+◊sub-heading{Abstract}
+
+Automated decision-making takes up an increasingly significant place
+in our administrative state. This article presents a conception of
+discretion that is helpful for evaluating the proper place of
+algorithms in public decision-making. I argue that the algorithm
+itself is not a site of discretion. The threat is that automated
+decision-making alters the relationships between traditional actors in
+a way that can cut down discretion and human commitment. Algorithmic
+decision-makers can serve to fetter the discretion that the
+legislature and the populace expect to be exercised. We must strive to
+maintain discretion, moral agency, deliberative ideals, and human
+commitment through the system that surrounds the use of an algorithm
+and to develop a new expertise that can retain and exercise the
+expected discretion. Backing this argument are traditional legal
+constraints, public expectations, and administrative law principles,
+tied together through the organizing principle of discretion.
+
+◊sub-heading{Thanks}
+
+I would like to thank Professor Mary Liston for supervising this
+research project and for our many engaging discussions. I also thank
+the participants of the Algorithms and Rule of Law reading group
+convened at the Peter A. Allard School of Law.
