99faa72b0161b96de16dd9cea453cb7647cc28bc
Sancho McCann <sanchom@gmail.com>
2023-02-01 16:46:00 -0500

    Updated some publication pages

--- AI-copyright.html.pm
+++ AI-copyright.html.pm
@@ -4,11 +4,12 @@
 ◊define-meta[short-title]{AI Copyright}
 ◊define-meta[snippet]{An article I wrote about copyright in the inputs, intermediate products, and final output of AI programs.}
 ◊define-meta[original-date]{2021-07-27}
-◊define-meta[edited-date]{2021-08-06}
+◊define-meta[edited-date]{2023-02-01}
 
 ◊declare-work[#:id "McCann" #:type "article" #:author "Sancho McCann"
 #:title "Copyright Throughout a Creative AI Pipeline" #:journal "Can
-JL & Tech" #:year "2021" #:volume "19" #:first-page "109"]
+JL & Tech" #:year "2021" #:volume "19" #:first-page "109" #:url
+"https://digitalcommons.schulichlaw.dal.ca/cjlt/vol19/iss1/5/"]
 
 ◊fig[#:src "assets/cortes-wheat.jpg"]{This is an image I created using
 the AI tool, ◊em{[Deep Dream
@@ -20,19 +21,8 @@ not claim ownership of the AI output.}
 
 My article, "Copyright Throughout a Creative AI
 Pipeline,"◊note-cite["McCann"] was just published by the Canadian
-Journal of Law & Technology. The final, reviewed and edited version is
-only accessible through ◊a[#:href
-"https://nextcanada.westlaw.com/Document/I17a8222eea2711ebbea4f0dc9fb69570/View/FullText.html?transitionType=Default&contextData=(sc.Default)&VR=3.0&RS=cblt1.0)"]{WestLaw}
-for the first nine months after publication. But I am allowed to share
-[this draft version on
-SSRN](https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3893972) for
-the time being. I also share an abstract below. If you have WestLaw
-access, I encourage you to read the final version; I think it was
-quite improved through the review and editing process.
-
-The draft version on SSRN does not include some of the
-acknowledgements and important references that made it into the final
-version, so I include those below.
+Journal of Law & Technology. It is available open-access
+[here](https://digitalcommons.schulichlaw.dal.ca/cjlt/vol19/iss1/5/).
 
 ◊sub-heading{Abstract}
 
@@ -133,58 +123,3 @@ preserving protection for original expression.
 I would like to thank Professor Jon Festinger, Q.C., for many helpful
 discussions while supervising this work and Professor Graham Reynolds
 for valuable feedback on an earlier draft.
-
-◊sub-heading{Related work}
-
-There are several related works that contribute to and are cited in
-the ◊a[#:href
-"https://nextcanada.westlaw.com/Document/I17a8222eea2711ebbea4f0dc9fb69570/View/FullText.html?transitionType=Default&contextData=(sc.Default)&VR=3.0&RS=cblt1.0)"]{final
-piece} that are not included in the [draft
-version](https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3893972)
-that I've shared for now. These were enlightening and make related
-arguments to mine, so I share them here as well.
-
-◊declare-work[#:id "IPQ" #:type "book" #:author "Margaret Llewelyn"
-#:editors? "yes" #:title "Intellectual Property Quarterly" #:publisher
-"Sweet & Maxwell" #:publisher-location "London, UK" #:year "2020"]
-
-◊format-work[#:type "chapter" #:author "Enrico Bonadio"
-#:author2-given "Luke" #:author2-family "McDonagh" #:title "Artificial
-Intelligence as Producer and Consumer of Copyright Works: Evaluating
-the Consequences of Algorithmic Creativity" #:in-book "IPQ"
-#:first-page "112" #:url
-"https://openaccess.city.ac.uk/id/eprint/23951/"]
-
-◊format-work[#:type "article" #:author-given "Bruce E" #:author-family
-"Boyden" #:title "Emergent Works" #:year "2016" #:journal "Colum JL &
-Arts" #:volume "39" #:issue "3" #:first-page "377" #:url
-"https://doi.org/10.7916/jla.v39i3.2077"]
-
-◊format-work[#:type "article" #:author "Carys Craig" #:author2-given
-"Ian" #:author2-family "Kerr" #:title "The Death of the AI Author"
-#:year "2021" #:journal "Ottawa L Rev" #:volume "52" #:issue "1"
-#:first-page "31" #:url "https://rdo-olr.org/fr/2021/5612/"]
-
-◊format-work[#:type "article" #:author-given "Daniel J"
-#:author-family "Gervais" #:title "The Machine as Author" #:journal
-"Iowa L Rev" #:year "2020" #:volume "105" #:issue "5" #:first-page
-"2053" #:url
-"https://ilr.law.uiowa.edu/print/volume-105-issue-5/the-machine-as-author/"]
-
-◊format-work[#:type "article" #:author "James Grimmelmann" #:title
-"There's No Such Thing as a Computer-Authored Work---And It's a Good
-Thing, Too" #:year "2016" #:journal "Colum JL & Arts" #:volume "39"
-#:issue "3" #:first-page "403" #:url
-"https://doi.org/10.7916/jla.v39i3.2079"]
-
-◊format-work[#:type "article" #:author-given "Mark A" #:author-family
-"Lemley" #:author2-given "Bryan" #:author2-family "Casey" #:title
-"Fair Learning" #:journal "Texas L Rev" #:volume "99" #:issue "4"
-#:first-page "743" #:year "2021" #:url
-"https://texaslawreview.org/fair-learning/"]
-
-◊format-work[#:type "article" #:author "Pamela Samuelson" #:title
-"Allocating Ownership Rights in Computer-Generated Works" #:journal "U
-Pitt L Rev" #:volume "47" #:first-page "1185" #:year "1986" #:url
-"https://lawcat.berkeley.edu/record/1112407"]
-
