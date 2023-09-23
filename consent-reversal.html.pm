c366af9bb2f48b6dccf43052de21b093d31f1d97
Sancho McCann <sanchom@gmail.com>
2023-09-23 11:23:37 -0700

    Consent reversals

---
+++ consent-reversal.html.pm
@@ -0,0 +1,295 @@
+#lang pollen
+
+◊define-meta[page-title]{◊@{Consenting to reverse under s. 70 of the ◊em{Supreme Court Act}}}
+◊define-meta[short-title]{Consent reversals}
+◊define-meta[snippet]{Consenting to reverse under s. 70 of the Supreme Court Act. What does this mean for precedent?}
+◊define-meta[original-date]{2023-09-23}
+
+◊declare-work[#:id "SCA" #:type "statute" #:title "Supreme Court Act"
+#:volume "R.S.C." #:year "1985" #:chapter "S-26" #:url
+"https://laws-lois.justice.gc.ca/eng/acts/s-26/FullText.html"]
+
+◊declare-work[#:id "SCA-1970" #:type "statute" #:title "Supreme Court Act"
+#:volume "R.S.C." #:year "1970" #:chapter "S-19"]
+
+◊declare-work[#:id "SCA-1952" #:type "statute" #:title "Supreme Court Act"
+#:volume "R.S.C." #:year "1952" #:chapter "259"]
+
+◊declare-work[#:id "SCA-1927" #:type "statute" #:title "Supreme Court Act"
+#:volume "R.S.C." #:year "1927" #:chapter "35"]
+
+◊declare-work[#:id "SCA-1906" #:type "statute" #:title "Supreme Court Act"
+#:volume "R.S.C." #:year "1906" #:chapter "139"]
+
+◊declare-work[#:id "SCA-1886" #:type "statute" #:title "The Supreme
+and Exchequer Courts Act" #:volume "R.S.C." #:year "1886" #:chapter
+"135"]
+
+◊declare-work[#:id "SCA-1875" #:type "statute" #:title "An Act to
+establish a Supreme Court, and a Court of Exchequer, for the Dominion
+of Canada" #:volume "38 Vic" #:year "(1875)" #:chapter "11"]
+
+The ◊em{Supreme Court Act}, s. 70 says:◊note{◊cite["SCA" #:pinpoint "s
+70"] Previously, until December 12, 1988, this was at ◊cite["SCA-1970"
+#:pinpoint "s. 75"] Before that, see ◊cite["SCA-1952" #:pinpoint
+"s. 75" #:terminal ";"] ◊cite["SCA-1927" #:pinpoint "s 76" #:terminal
+";"] ◊cite["SCA-1906" #:pinpoint "s 81" #:terminal ";"]
+◊cite["SCA-1886" #:pinpoint "s 52"] It was originally introduced in
+◊cite["SCA-1875" #:pinpoint "s 40"]}
+
+◊q{ A respondent may consent to the reversal of the judgment appealed
+against by giving to the appellant a notice entitled in the Court and
+in the cause, and signed by the respondent or the respondent’s
+attorney or solicitor, stating that the respondent consents to the
+reversal of the judgment, and thereupon the Court or a judge shall
+pronounce judgment of reversal as of course. }
+
+On September 21, 2023, the Supreme Court of Canada granted such a
+reversal in ◊em{Licensing IP International S.À.R.L., et al. v. Sweet
+Productions Inc., et al.} (File
+No. [40354](https://www.scc-csc.ca/case-dossier/info/dock-regi-eng.aspx?cas=40354)):
+
+◊q{
+
+The application for leave to appeal from the judgment of the Federal
+Court of Appeal, Number A-100-21, 2022 FCA 111, dated June 10, 2022,
+is granted.
+
+The appellants apply for an order, pursuant to s. 70 of the
+◊em{Supreme Court Act}, reversing the judgment of the Federal Court of
+Appeal dated June 10, 2022, and restoring the judgment of the Federal
+Court, Number T-1440-19, 2021 FC 216, dated March 10, 2021.
+
+The appellants bring the motion under s. 70 of the ◊em{Supreme Court Act}
+on the basis that the parties have settled their dispute and have made
+a reversal on consent order a condition of their settlement.
+
+Upon reading the materials filed by the parties, and noting the
+consent of the respondents to a reversal of the judgment pursuant to
+s. 70 of the ◊em{Supreme Court Act};
+
+IT IS HEREBY ORDERED THAT:
+
+The motion to reverse the judgment of the Federal Court of Appeal is
+granted. The judgment of the Federal Court of Appeal bearing court
+file number A-100-21, 2022 FCA 111, and dated June 10, 2022, is set
+aside, and the judgment of the Federal Court, bearing court file
+number T-1440-19, 2021 FC 216, dated March 10, 2021, is restored.
+
+Judgment accordingly
+
+}
+
+◊sub-heading{How rare is this procedure?}
+
+[Tom Slade
+says](https://twitter.com/thomasslade/status/1704869901050003579),
+"Very Rare!" [Bruce Ryder
+says](https://x.com/BBRyder/status/1704911313766367694), "How
+unusual." [Paul-Erik
+Veel](https://x.com/PaulErikVeel/status/1704951493273493669) had not
+needed to code for this event in the [Lenczner Slaght Supreme Court of
+Canada Database](https://supremecourtdatabase.com/) until now.
+
+Searching CanLII for ◊a[#:href
+"https://www.canlii.org/en/#search/type=decision&startDate=1988-01-01&endDate=2023-12-31&sort=decisionDateAsc&text=%22Supreme%20Court%20Act%E2%80%9D%20%2Fs%20%E2%80%9Cs.%2070%22"]{◊tt{"Supreme
+Court Act" /s "s. 70"} since 1988} reveals four instances (other than
+the one from this week):
+
+◊declare-work[#:id "Reddick1" #:type "legal-case" #:title "R v Reddick"
+#:citation "[1991] 1 SCR 297" #:url "https://canlii.ca/t/1fsl2" #:short-form "*Reddick* 1"]
+
+◊declare-work[#:id "Reddick2" #:type "legal-case" #:title "R v Reddick"
+#:citation "[1991] 1 SCR 1105" #:url "https://canlii.ca/t/1fsl2" #:short-form "*Reddick* 2"]
+
+◊declare-work[#:id "Mills" #:type "legal-case" #:title "R v Mills"
+#:citation "[1995] 1 SCR 902" #:url "https://canlii.ca/t/1frm0"
+#:short-form "Mills"]
+
+◊declare-work[#:id "Shorting" #:type "legal-case" #:title "R v
+Shorting" #:year "1996" #:citation "110 CCC (3d) 383" #:url
+"https://canlii.ca/t/gbgr2" #:short-form "Shorting"]
+
+◊declare-work[#:id "RAL" #:type "legal-case" #:title "R v ALR" #:year
+"2001" #:citation "[2001] 11 WWR 413" #:jurisdiction "SCC"
+#:parallel-citation "Bulletin of Proceedings of the Supreme Court of
+Canada, March 2, 2001" #:url "https://canlii.ca/t/gbtnr"]
+
+◊itemize{
+
+◊cite["Reddick2"]
+
+◊cite["Mills"]
+
+◊cite["Shorting"]
+
+◊cite["RAL"]
+
+}
+
+And searching CanLII for ◊a[#:href
+"https://www.canlii.org/en/#search/type=decision&startDate=1952-01-01&endDate=1988-12-31&sort=decisionDateAsc&text=%22Supreme%20Court%20Act%E2%80%9D%20%2Fs%20%E2%80%9Cs.%2075%22"]{◊tt{"Supreme
+Court Act" /s "s. 75"} between 1952 and 1988} reveals two instances:
+
+◊declare-work[#:id "Jackson" #:type "legal-case" #:title "Jackson v
+The Queen" #:citation "[1979] 2 SCR 707" #:url
+"https://canlii.ca/t/1z78c"]
+
+◊declare-work[#:id "CNR" #:type "legal-case" #:title "Canadian Pacific
+Ltd v Drumheller (City)" #:citation "[1988] 1 SCR 1901" #:url
+"https://canlii.ca/t/1ftcg"]
+
+◊itemize{
+
+◊cite["Jackson"]
+
+◊cite["CNR"]
+
+}
+
+
+However, it is clear there are more of these reversals than appear in
+CanLII's dataset. For instance, in ◊em{Reddick}, Justice Sopinka noted
+that "[a] similar order granting a judgment of reversal was given by
+McLachlin J. in ◊em{R. v. McCaul}, Bulletin of Proceedings of the
+Supreme Court of Canada, September 27, 1991, at p. 1995 (judgment
+rendered October 1, 1990)." You can see that the Court made the s. 70
+order in ◊em{McCaul} by looking at the docket (File
+No. [21634](https://scc-csc.ca/case-dossier/info/dock-regi-eng.aspx?cas=21634)).
+
+◊declare-work[#:id "GarchaCA" #:type "legal-case" #:title "R v Garcha"
+#:citation "2000 BCCA 550" #:url "https://canlii.ca/t/1fngr" #:short-form "*Garcha* CA"]
+
+◊declare-work[#:id "GarchaSCC" #:type "magazine/news"
+#:title "Bulletin of Proceedings of the Supreme Court of Canada"
+#:date "16 February 2001" #:url
+"https://decisions.scc-csc.ca/scc-csc/bulletins/en/298/1/document.do"
+#:short-form "*Garcha* SCC"]
+
+◊sub-heading{How are consent reversals used?}
+
+A good example is ◊em{R. v. Garcha}.◊note-cite["GarchaCA"
+#:parenthetical "the reversal is not found in CanLII"] In ◊em{Garcha},
+the appellant was convicted of a number of offences, including the use
+of an imitation firearm while committing a robbery. The appellant's
+appeal to the Court of Appeal was unsuccessful, and the formal order
+dismissing the appeal was entered on March 20, 2000. ◊em{After} the
+formal order was entered, the Crown advised the Court of Appeal that
+the imitation-firearm offence was not yet in force on the date of the
+offence: the appellant should not have been convicted of that
+count. The Court of Appeal said it did not have jurisdiction to
+re-open the appeal but noted that the appellant could use s. 70 of the
+◊em{Supreme Court Act}. The Crown said that it would consent to the
+reversal in relation to the imitation-firearm count. The appellant
+appealed to the Supreme Court of Canada and brought a s. 70 motion in
+relation to Count 8, to which the Crown consented. The Supreme Court
+of Canada allowed the motion and entered an order reversing the Court
+of Appeal judgment in part:◊note-cite["GarchaSCC"]
+
+◊q{
+
+The portion of the judgment of the Court of Appeal for British
+Columbia dated March 10, 2000 dismissing the appellant's appeal from
+his conviction on Count Eight for use of an imitation firearm on or
+about the 30th day of December, 1995, while committing or attempting
+to commit the indictable offence of robbery, contrary to s. 85(2) of
+the ◊em{Criminal Code of Canada}, R.S.C. 1985, c. C-46, is reversed
+and an acquittal of the charge in Count Eight is entered in
+substitution thereof.
+
+}
+
+I found one instance where the Court declined to order
+reversal.◊note-cite["Reddick1"] In ◊em{Reddick} 1, the appellant was
+seeking three convictions to be quashed, but the Crown respondent was
+consenting to "reversal of the Judgment, to the extent that a new
+trial be ordered." The Court noted that the section "does not require
+the consent of the appellant but rather contemplates a situation in
+which the respondent consents to a disposition that gives the
+appellant ◊em{all the relief that would be available if complete
+success were achieved on the appeal}." The Court declined to let the
+Crown consent to a disposition that would unilaterally deprive the
+appellant of the outcome he was seeking. A few months later, the
+appellant consented to a new trial and the Supreme Court of Canada
+allowed the new s. 70 motion to reverse the judgment of the Appeal
+Division of the Supreme Court of Nova Scotia and to order a new
+trial.◊note-cite["Reddick2"]
+
+Under s. 70, the Supreme Court of Canada has:
+
+◊itemize{
+
+allowed partial reversal of a judgment;◊note-cite["GarchaSCC"]
+
+imposed alternative orders that might have been given its place,
+including an order for a new trial◊note-cite["Reddick2"] and an order
+of acquittal;◊note-cite["GarchaSCC"] and
+
+simply restored the order of the trial court.◊note-cite["CNR"]
+
+}
+
+
+◊sub-heading{What precedential weight should be given to the reasons
+for a judgment reversed by consent?}
+
+◊declare-work[#:id "Sullivan" #:type "legal-case" #:title "R v
+Sullivan" #:citation "2022 SCC 19" #:url
+"https://canlii.ca/t/jp64b#par56"]
+
+◊declare-work[#:id "RA" #:type "legal-case" #:title "R v A" #:citation
+"[1990] 1 SCR 992" #:url "https://canlii.ca/t/1fsxf"]
+
+◊declare-work[#:id "CIBC" #:type "legal-case" #:title "Canadian
+Imperial Bank of Commerce v Bury" #:citation "103 DLR (3d) 560" #:year
+"1979" #:jurisdiction "Alta. Q.B."]
+
+First, I acknowledge there is ambiguity or conflation of
+terminology. But, the Supreme Court of Canada has occasionally
+distinguished "judgment" and "reasons for judgment."◊note-cite["RA"]
+In the context of the ◊em{Supreme Court Act}, ◊em{judgment} is roughly
+synonymous with ◊em{order}.◊note{The ◊em{Supreme Court Act} defines
+judgment to ◊em{include} order. There are other settings in which the
+distinction may matter. See e.g. ◊cite["CIBC" #:parenthetical
+"distinguishing between order and judgment but conflating neither with
+the reasons"]} Appeals are from orders (judgments) and the disposition
+of an appeal turns on what the reviewing court does to the order
+(judgment). A reviewing court can completely disagree with a lower
+court's reasoning, yet dismiss an appeal if the reviewing court leaves
+in place the lower court's order (judgment) on other grounds. Or a
+reviewing court may not even have turned its mind to the reasoning,
+yet reverse a judgment, as the Supreme Court of Canada does in a s. 70
+consent reversal.
+
+◊em{Stare decisis} "pertains to the reasons given by a
+court."◊note-cite["Sullivan" #:pinpoint "para. 56"] Certainly,
+reversing the formal judgment has significance for the parties to the
+actual case under appeal: a person might be acquitted or receive a new
+trial. But the ◊em{reasons} are often left unimpugned by a consent
+reversal. Those reasons speak beyond the particular case.
+
+Courts frequently rely on reasons for points of law, even though the
+ultimate judgment may have been reversed "on other grounds." A consent
+reversal is just a special case of this more general occurrence.
+
+◊declare-work[#:id "Sweet" #:type "legal-case" #:title "Sweet
+Productions Inc et al v Licensing LP International SÀRL et al"
+#:citation "2022 FCA 111" #:url "https://canlii.ca/t/jprg3"
+#:short-form "Sweet Productions"]
+
+For example, the Federal Court of Appeal, in ◊em{Sweet Productions
+Inc.},◊note-cite["Sweet" #:parenthetical "reversed by consent, 21
+September 2023"] rejected the Federal Court's interpretation that Rule
+167 of the ◊em{Federal Court Rules} creates a presumption of dismissal
+when undue delay is found. Instead, the Federal Court of Appeal held
+that Rule 167 "grants the Court a wide discretion to craft the remedy
+that is appropriate in the circumstances of each case." It cannot be
+that litigants and judges are free to ignore this reasoning just
+because the ultimate judgment was set aside by a s. 70 consent
+reversal.
+
+In my view, the reasons of a Court of Appeal judgment that has been
+reversed on consent should continue to be considered binding precedent
+through the ordinary principles of vertical ◊em{stare decisis}, unless
+the Supreme Court of Canada expressly or implicitly disavows the
+reasoning.
